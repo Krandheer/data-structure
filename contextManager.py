cfe4cb3c6d1dacef8434c23618687952d1c168bf
randheer <randheer.dakshana15@gmail.com>
2021-02-08 11:01:26 +0530

    context manager

---
+++ contextManager.py
@@ -0,0 +1,12 @@
+from contextlib import contextmanager
+
+@contextmanager
+def open_file_name(fileName):
+	f=open('fileName', 'w')
+	try:
+		yield f
+	finally:
+		f.close()
+
+with open_file_name('name.txt') as f:
+	f.write('things to do')
