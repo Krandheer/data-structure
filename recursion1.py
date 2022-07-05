1e350e4101b1881c71e00eacd50fa83e126de8d0
randheer <randheer.dakshana15@gmail.com>
2022-07-05 11:12:44 +0530

    recursion1

---
+++ recursion1.py
@@ -0,0 +1,21 @@
+def print_n(n):
+    if n == 0:
+        return
+    print(n)
+    print_n(n - 1)
+    print(n)
+
+
+def factorial(n):
+    if n == 1:
+        return n
+    return n * factorial(n - 1)
+
+
+def sum_of_digits(n):
+    if n == 0:
+        return 0
+    return n % 10 + sum_of_digits(n // 10)
+
+
+print(sum_of_digits(257))
