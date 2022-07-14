0ca8f07f16559cc33896cebc0e95297defeb75e9
randheer <randheer.dakshana15@gmail.com>
2022-07-14 18:51:19 +0530

    m

--- selecton_sort.py
+++ selecton_sort.py
@@ -1,11 +1,10 @@
 def selection_sort(alist):
-	for num_pass in range(len(alist)-1, 0, -1):
-		position_of_max = 0
-		for i in range(1,num_pass+1):
-			if (alist[i]>alist[position_of_max]):
-				position_of_max = i
+    for num_pass in range(len(alist) - 1, 0, -1):
+        position_of_max = 0
+        for i in range(1, num_pass + 1):
+            if alist[i] > alist[position_of_max]:
+                position_of_max = i
 
-		alist[num_pass],alist[position_of_max]=alist[position_of_max],alist[num_pass]
-	
+        alist[num_pass], alist[position_of_max] = alist[position_of_max], alist[num_pass]
 
-	return alist
+    return alist
