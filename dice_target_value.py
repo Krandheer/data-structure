b5581c2f847bccd63234a63306ed2cfe07bb450c
randheer <randheer.dakshana15@gmail.com>
2022-07-15 20:39:34 +0530

    find the way to make a sum using number less than the target

---
+++ dice_target_value.py
@@ -0,0 +1,31 @@
+"""
+recursion solution to find the way to get the to the particular number on dice
+"""
+
+
+def dice(pat, target):
+    if target == 0:
+        print(pat)
+    for i in range(1, target + 1):
+        dice(pat + str(i), target - i)
+
+
+def dice2(pat, target, result):
+    if target == 0:
+        result.append(pat)
+    for i in range(1, target + 1):
+        dice2(pat + str(i), target - i, result)
+    return result
+
+
+def dice3(pat, target, result):
+    if target == 0:
+        result.append(pat)
+    for i in range(1, target + 1):
+        dice3(pat + str(i), target - i, result)
+    return len(result)
+
+
+dice("", 4)
+print(dice2("", 4, []))
+print(dice3("", 4, []))
