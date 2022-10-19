d8bb3451452a94ff6e60162cd5f9064fb22449b7
Randheer kumar <randheer.dakshana15@gmail.com>
2022-10-20 00:26:56 +0530

    recursive method also added

--- binary_search.py
+++ binary_search.py
@@ -10,6 +10,28 @@ def binary_search(sorted_list, x):
         return i
 
 
+"""
+recursive binary search
+"""
+
+
+def recursive_binary_search(sorted_list, x):
+    if len(sorted_list) == 0:
+        return False
+
+    else:
+        midpoint = len(sorted_list) // 2
+        if sorted_list[midpoint] == x:
+            return True
+        else:
+            if sorted_list[midpoint] < x:
+                return recursive_binary_search(sorted_list[midpoint + 1:], x)
+
+            else:
+                return recursive_binary_search(sorted_list[:midpoint], x)
+
+
 a = [1, 2, 4, 6, 7, 12, 34, 39, 54]
 
 print(binary_search(a, 54))
+print(recursive_binary_search(a, 54))
