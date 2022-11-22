ead92ad55df088e5950ef250e28b3b083ffa8451
Randheer kumar <randheer.dakshana15@gmail.com>
2022-11-22 16:40:38 +0530

    modified

--- number_of_zeros.py
+++ number_of_zeros.py
@@ -1,16 +1,27 @@
-count = 0
+# count = 0
+#
+#
+# def get_zeros(n):
+#     global count
+#     if n == 0:
+#         return 1
+#     if n % 10 == 0:
+#         count = count + 1
+#         get_zeros(n // 10)
+#     else:
+#         get_zeros(n // 10)
+#
+#
+# get_zeros(40304)
+# print(count)
 
-
-def get_zeros(n):
-    global count
+def get_zeros(n, count):
     if n == 0:
-        return 1
+        return count
     if n % 10 == 0:
-        count = count + 1
-        get_zeros(n // 10)
+        return get_zeros(n // 10, count + 1)
     else:
-        get_zeros(n // 10)
+        return get_zeros(n // 10, count)
 
 
-get_zeros(40304)
-print(count)
+print(get_zeros(40304040, 0))
