708c6e3bad53af89d334d4af1c8916914348fe0b
Randheer kumar <randheer.dakshana15@gmail.com>
2022-11-25 22:17:14 +0530

    rec1 commit

---
+++ rec1.py
@@ -0,0 +1,53 @@
+def waystoclimstairs(p, target, ans):
+    if target == 0:
+        ans.append(p)
+        return
+
+    waystoclimstairs(p + str(1), target - 1, ans)
+    if target >= 2:
+        waystoclimstairs(p + str(2), target - 2, ans)
+    if target >= 3:
+        waystoclimstairs(p + str(3), target - 3, ans)
+    return len(ans)
+
+
+print(waystoclimstairs('', 10, []))
+
+def swap(arr):
+    arr1 = []
+    for i in range(len(arr) - 1, -1, -1):
+        arr1.append(arr[i])
+    return arr1
+
+
+# print(swap([1, 1, 0, 0, 1]))
+
+def horizontal_flip(mat):
+    mat1 = []
+    mat2 = []
+    for row in mat:
+        mat2.append([])
+    for row in mat:
+        temp = swap(row)
+        mat1.append(temp)
+    for index, row in enumerate(mat1):
+        for i in row:
+            if i == 0:
+                i = 1
+                mat2[index].append(i)
+            elif i == 1:
+                i = 0
+                mat2[index].append(i)
+    return mat2
+
+
+# img = [[1, 1, 0, 0, 1],
+#        [0, 0, 1, 0, 1],
+#        [1, 1, 1, 0, 1]]
+# print(horizontal_flip(img))
+
+img2 = [[1, 1, 0],
+        [1, 1, 1],
+        [0, 0, 1],
+        [1, 0, 1]]
+print(horizontal_flip(img2))
