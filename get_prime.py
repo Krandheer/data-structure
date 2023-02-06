9f4548fadac45962806b290d105888f51bb24ee3
Randheer kumar <randheer.dakshana15@gmail.com>
2023-02-06 19:10:44 +0530

    optimised

--- get_prime.py
+++ get_prime.py
@@ -15,9 +15,7 @@ def get_prime(n):
 def is_prime(n):
     if n <= 1:
         return False
-    if n == 2:
-        return True
-    for i in range(2, math.ceil(math.sqrt(n)) + 1):
+    for i in range(2, math.floor(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
