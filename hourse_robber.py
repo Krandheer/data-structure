5c08bdfd97e8fed64f90f618a1e21756f627f643
Randheer kumar <randheer.dakshana15@gmail.com>
2023-02-19 12:49:49 +0530

    leetcode dp

--- hourse_robber.py
+++ hourse_robber.py
@@ -14,3 +14,20 @@ def rob(nums):
         dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
 
     return dp[len(nums) - 1]
+
+
+class Solution:
+    def rob(self, nums) -> int:
+        return self.rob2(len(nums) - 1, nums, [-1] * (len(nums)))
+
+    def rob2(self, i, nums, dp):
+        if i == 0:
+            return nums[i]
+        elif i < 0:
+            return 0
+        if dp[i] != -1:
+            return dp[i]
+        pick = nums[i] + self.rob2(i - 2, nums, dp)
+        unpick = 0 + self.rob2(i - 1, nums, dp)
+        dp[i] = max(pick, unpick)
+        return dp[i]
