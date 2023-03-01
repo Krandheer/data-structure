d75b8f03009e07b8e69d70a6fa1b41fb2c2ee9b8
Randheer kumar <randheer.dakshana15@gmail.com>
2023-03-01 11:20:04 +0530

    leetcode solution

---
+++ maximum_product_subarray.py
@@ -0,0 +1,16 @@
+def maxProduct(nums) -> int:
+    """
+    leetcode: we maintain curMax and curMax to handle the case in which product of -ve and -ve becomes positive.
+    """
+    curMax, curMin = 1, 1
+    res = max(nums)
+
+    for n in nums:
+        if n == 0:
+            curMax, curMin = 1, 1
+        vals = (n, n * curMax, n * curMin)
+        curMax, curMin = max(vals), min(vals)
+
+        res = max(res, curMax)
+
+    return res
