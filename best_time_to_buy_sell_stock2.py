66ab54b52d3d5994bc880016f13411146d41094d
Randheer kumar <randheer.dakshana15@gmail.com>
2023-03-05 17:30:11 +0530

    stocks buy sell dp

--- best_time_to_buy_sell_stock2.py
+++ best_time_to_buy_sell_stock2.py
@@ -1,11 +1,36 @@
-def maxProfit(prices):
+# def maxProfit(prices):
+#     """
+#     leetcode buy sell stocks part II
+#     """
+#     ans = 0
+#     for i in range(len(prices) - 1):
+#         if prices[i + 1] > prices[i]:
+#             ans = ans + (prices[i + 1] - prices[i])
+#     return ans
+#
+#
+# print(maxProfit([4, 9, 0, 4, 10]))
 
-    """
-    leetcode buy sell stocks part II
-    """
-    ans = 0
-    for i in range(len(prices) - 1):
-        if prices[i + 1] > prices[i]:
-            ans = ans + (prices[i + 1] - prices[i])
-    return ans
 
+# using dp take or not take logic, if buy then that means I am giving money and if bought then set buy to 1, so that
+# it indicates that we can't buy till we sell and make buy to 1, and each time we call function we move one index
+def maxProfit(prices) -> int:
+    dp = [[-1, -1] for i in range(len(prices))]
+    return helper(prices, 0, 0, dp)
+
+
+def helper(prices, index, buy, dp):
+    if index >= len(prices):
+        return 0
+    if dp[index][buy] != -1:
+        return dp[index][buy]
+    if not buy:
+        dp[index][buy] = max(-prices[index] + helper(prices, index + 1, 1, dp),
+                             helper(prices, index + 1, 0, dp))
+    elif buy:
+        dp[index][buy] = max(prices[index] + helper(prices, index + 1, 0, dp),
+                             helper(prices, index + 1, 1, dp))
+    return dp[index][buy]
+
+
+print(maxProfit([4, 9, 0, 4, 10]))
---
+++ best_time_to_buy_stocks3.py
@@ -0,0 +1,22 @@
+def maxProfit(prices) -> int:
+    """
+    using dp take or not take logic, if buy then that means I am giving money and if bought then set buy to 1, so that
+    it indicates that we can't buy till we sell and make buy to 1, and each time we sell and call function then
+    we move two point ahead in index value for taking care of cooldown period.
+    """
+    dp = [[-1, -1] for i in range(len(prices))]
+    return helper(prices, 0, 0, dp)
+
+
+def helper(prices, index, buy, dp):
+    if index >= len(prices):
+        return 0
+    if dp[index][buy] != -1:
+        return dp[index][buy]
+    if not buy:
+        dp[index][buy] = max(-prices[index] + helper(prices, index + 1, 1, dp),
+                             helper(prices, index + 1, 0, dp))
+    elif buy:
+        dp[index][buy] = max(prices[index] + helper(prices, index + 2, 0, dp),
+                             helper(prices, index + 1, 1, dp))
+    return dp[index][buy]
