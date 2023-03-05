2baa4368fe90e114184ed32080f6dd4d6e3f70ab
Randheer kumar <randheer.dakshana15@gmail.com>
2023-03-05 20:20:36 +0530

    leetcode more problems

---
+++ stocks4.py
@@ -0,0 +1,20 @@
+def maxProfit(prices, fee):
+    """
+    same as third one except that here one complete transaction has some fee and cool down period is not needed.
+    """
+    dp = [[-1, -1] for i in range(len(prices))]
+    return helper(prices, 0, 0, dp, fee)
+
+
+def helper(prices, index, buy, dp, fee):
+    if index >= len(prices):
+        return 0
+    if dp[index][buy] != -1:
+        return dp[index][buy]
+    if not buy:
+        dp[index][buy] = max(-prices[index] + helper(prices, index + 1, 1, dp, fee),
+                             helper(prices, index + 1, 0, dp, fee))
+    elif buy:
+        dp[index][buy] = max(prices[index] - fee + helper(prices, index + 2, 0, dp, fee),
+                             helper(prices, index + 1, 1, dp, fee))
+    return dp[index][buy]
---
+++ word_break.py
@@ -0,0 +1,21 @@
+"""
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into
+a space-separated sequence of one or more dictionary words.
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+"""
+
+
+def wordBreak(s: str, wordDict) -> bool:
+    dp = [False for i in range(len(s) + 1)]
+    dp[0] = True
+
+    for i in range(len(s)):
+        # start from string and then from that index check for words and if words found then last index of that word
+        # becomes starting for next inner loop
+        for j in range(i, len(s)):
+            if not dp[i]:
+                break
+            if dp[i] and s[i:j + 1] in wordDict:
+                dp[j + 1] = True
+
+    return dp[-1]
