476843e1dc7195b2c1c2899658ec0efb8fb877c8
Randheer kumar <randheer.dakshana15@gmail.com>
2023-03-07 16:23:48 +0530

    unique binary search trees

---
+++ unique_binary_search_trees.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        return self.fact(2 * n) // self.fact(n) // self.fact(n) // (n + 1)
+
+    def fact(self, n):
+        if n == 0 or n == 1:
+            return 1
+        temp = 1
+        for i in range(1, n + 1):
+            temp = temp * i
+        return temp
+
+
+sol = Solution()
+print(sol.fact(3))
