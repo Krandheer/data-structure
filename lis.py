33d955f3cadeaf1364f361e9594f006bd4563422
Randheer kumar <randheer.dakshana15@gmail.com>
2023-03-08 14:24:24 +0530

    longest increasing subsequence

--- lis.py
+++ lis.py
@@ -1,9 +1,13 @@
 def lis_recursion(index, prev_index, arr):
+    # prev index to know if previous value was taken or not, if taken then compare that current value should be
+    # greater than previous value. start with minus one, as initially no value will be taken.
+    # prev index is update only when another value is taken that justify the condition of increasing sequence.
+    # and this happens for all values, so all subsequence is considered
     if index == len(arr):
         return 0
-    max_length = 0 + lis(index + 1, prev_index, arr)
+    max_length = 0 + lis_recursion(index + 1, prev_index, arr)
     if prev_index == -1 or arr[prev_index] < arr[index]:
-        max_length = max(max_length, 1 + lis(index + 1, index, arr))
+        max_length = max(max_length, 1 + lis_recursion(index + 1, index, arr))
     return max_length
 
 
@@ -11,7 +15,7 @@ def lis(index, prev_index, arr, dp):
     if index == len(arr):
         return 0
     if dp[index][prev_index + 1] != -1:
-        return dp[index][prev_index]
+        return dp[index][prev_index+1]
     max_length = 0 + lis(index + 1, prev_index, arr, dp)
     if prev_index == -1 or arr[prev_index] < arr[index]:
         max_length = max(max_length, 1 + lis(index + 1, index, arr, dp))
@@ -20,6 +24,7 @@ def lis(index, prev_index, arr, dp):
     return dp[index][prev_index + 1]
 
 
-arr = [30, 40, 2, 5, 1, 7, 45, 50, 8]
+arr = [30, 2, 5, 7, 45, 8]
+print(lis_recursion(0, -1, arr))
 dp = [[-1] * len(arr) for i in range(len(arr) + 1)]
-print(lis(0, -1, arr, dp))
+# print(lis(0, -1, arr, dp))
