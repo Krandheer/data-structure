5161a366a0521c3f533e63bfa254d32e872c466b
Randheer kumar <randheer.dakshana15@gmail.com>
2023-03-11 22:47:59 +0530

    changes

--- read_wrong_id.py
+++ read_wrong_id.py
@@ -7,7 +7,7 @@ def get_wrong_id_json_file(atm_id_to_process):
     json_file = {
         "filesToProcess": []
     }
-    path = "~/Downloads/Writer_Mongo_Data_Report_2023-02-22_2023-02-22T19_30_00.943Z.csv"
+    path = "~/Downloads/Writer_Mongo_Data_Report_2023-02-21_2023-02-21T19_30_00.398Z.csv"
     df = pd.read_csv(path, low_memory=False)
     atm_id = df[(df['ALL_FILE_PASS'] == "False")]['ATMID']
     atm_id = atm_id[atm_id.str[:4] == atm_id_to_process]
@@ -44,31 +44,31 @@ def get_top_fails():
             print(item)
 
 get_top_fails()
-# path = "~/Downloads/Writer_Mongo_Data_Report_2023-01-11_2023-01-11T19_30_00.260Z.csv"
-# df = pd.read_csv(path, low_memory=False)
-#
-#
-# atm_ids = ['S1AC', 'S1BW', 'S1AW', 'S1BB', 'S1CN', 'S1NW', 'S1NB', 'MPB0']
-# result = {}
-# for ids in atm_ids:
-#     temp_df = df[df.ATMID.str[:4] == ids]
-#     df_1 = temp_df[((temp_df['CA-FILE_PASS'] == "True") & (temp_df['CB-FILE_PASS'] == "True")) & (
-#             (temp_df['SA-FILE_PASS'] == "False") | (temp_df['SB-FILE_PASS'] == "False"))]
-#     df_2 = temp_df[((temp_df['CA-FILE_PASS'] == "True") | (temp_df['CB-FILE_PASS'] == "True")) & (
-#             (temp_df['SA-FILE_PASS'] == "False") & (temp_df['SB-FILE_PASS'] == "False"))]
-#
-#     atm_id_sa_sb = []
-#     atm_id_ca_cb = []
-#     for i in df_1['ATMID']:
-#         atm_id_sa_sb.append(i)
-#     for i in df_2['ATMID']:
-#         atm_id_ca_cb.append(i)
-#
-#     result[f"{ids}_sa_sb"] = atm_id_sa_sb
-#     result[f"{ids}_ca_cb"] = atm_id_ca_cb
-#
-# for key, val in result.items():
-#     print(key, len(val))
+path = "~/Downloads/Writer_Mongo_Data_Report_2023-02-21_2023-02-21T19_30_00.398Z.csv"
+df = pd.read_csv(path, low_memory=False)
+
+
+atm_ids = ['S1AC', 'S1BW', 'S1AW', 'S1BB', 'S1CN', 'S1NW', 'S1NB', 'MPB0']
+result = {}
+for ids in atm_ids:
+    temp_df = df[df.ATMID.str[:4] == ids]
+    df_1 = temp_df[((temp_df['CA-FILE_PASS'] == "True") & (temp_df['CB-FILE_PASS'] == "True")) & (
+            (temp_df['SA-FILE_PASS'] == "False") | (temp_df['SB-FILE_PASS'] == "False"))]
+    df_2 = temp_df[((temp_df['CA-FILE_PASS'] == "True") | (temp_df['CB-FILE_PASS'] == "True")) & (
+            (temp_df['SA-FILE_PASS'] == "False") & (temp_df['SB-FILE_PASS'] == "False"))]
+
+    atm_id_sa_sb = []
+    atm_id_ca_cb = []
+    for i in df_1['ATMID']:
+        atm_id_sa_sb.append(i)
+    for i in df_2['ATMID']:
+        atm_id_ca_cb.append(i)
+
+    result[f"{ids}_sa_sb"] = atm_id_sa_sb
+    result[f"{ids}_ca_cb"] = atm_id_ca_cb
+
+for key, val in result.items():
+    print(key, len(val))
 
 # df = df[df.ATMID.str[:4] == "S1BB"]
 # df = df[((df['CA-FILE_PASS'] == "True") & (df['CB-FILE_PASS'] == "True")) & (
