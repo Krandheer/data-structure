0bd847ea09a38be01f428a90da83357c72c2067a
Randheer kumar <randheer.dakshana15@gmail.com>
2023-03-12 14:23:41 +0530

    dp added

--- matrix_chain_multiplication.py
+++ matrix_chain_multiplication.py
@@ -27,6 +27,7 @@ def matrix_chain_mul_dp(i, j, arr, dp):
     min_steps = math.inf
     if dp[i][j] != -1:
         return dp[i][j]
+    # i = 0 to n - 1, j = n-1 to 1
     for k in range(i, j):
         steps = arr[i - 1] * arr[k] * arr[j] + matrix_chain_mul_dp(i, k, arr, dp) + matrix_chain_mul_dp(k + 1, j,
                                                                                                         arr, dp)
@@ -36,5 +37,19 @@ def matrix_chain_mul_dp(i, j, arr, dp):
     return dp[i][j]
 
 
-dp = [[-1 for j in range(len(arr))] for i in range(len(arr) - 1)]
-print(matrix_chain_mul_dp(1, len(arr) - 1, arr, dp))
+dp_1 = [[-1 for j in range(len(arr))] for i in range(len(arr) - 1)]
+print(matrix_chain_mul_dp(1, len(arr) - 1, arr, dp_1))
+
+
+# todo: this has bugs need to fix
+def matrix_chain_bottom_up(i, j, arr):
+    dp = [[0 for j in range(len(arr))] for i in range(len(arr) - 1)]
+    min_steps = math.inf
+    for k in range(i, len(arr) - 1):
+        steps = arr[i - 1] * arr[k] * arr[j] + dp[i][k] + dp[k + 1][j]
+        if steps < min_steps:
+            dp[i][k] = steps
+    return dp[1][len(arr) - 1]
+
+
+print(matrix_chain_bottom_up(1, len(arr) - 1, arr))
