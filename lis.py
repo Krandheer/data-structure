8b0b6e47ab0d6b712dc1e0db495d4a60363a9d86
Randheer kumar <randheer.dakshana15@gmail.com>
2023-03-29 20:15:01 +0530

    bottom up tabulation added

--- lis.py
+++ lis.py
@@ -17,7 +17,7 @@ def lis(index, prev_index, arr, dp):
     if index == len(arr):
         return 0
     if dp[index][prev_index + 1] != -1:
-        return dp[index][prev_index+1]
+        return dp[index][prev_index + 1]
     max_length = 0 + lis(index + 1, prev_index, arr, dp)
     if prev_index == -1 or arr[prev_index] < arr[index]:
         max_length = max(max_length, 1 + lis(index + 1, index, arr, dp))
@@ -26,8 +26,21 @@ def lis(index, prev_index, arr, dp):
     return dp[index][prev_index + 1]
 
 
+def list_bottom_up(nums):
+    n = len(nums)
+    dp = [[0 for i in range(n + 1)] for j in range(n + 1)]
+    for index in range(n - 1, -1, -1):
+        for prev_index in range(index - 1, -2, -1):
+            max_length = 0 + dp[index + 1][prev_index+1]
+            if prev_index == -1 or nums[prev_index] < nums[index]:
+                max_length = max(max_length, 1 + dp[index + 1][index+1])
+
+            dp[index][prev_index + 1] = max_length
+    return dp[0][0]
+
+
 arr = [30, 2, 5, 7, 45, 8]
 print(lis_recursion(0, -1, arr))
 dp = [[-1] * len(arr) for i in range(len(arr) + 1)]
 print(lis(0, -1, arr, dp))
-print(dp)
+print(list_bottom_up(arr))
