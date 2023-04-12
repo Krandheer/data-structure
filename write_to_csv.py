0d54990414d126b62e039b6a39d6e227e360f0d1
Randheer kumar <randheer.dakshana15@gmail.com>
2023-04-12 18:38:53 +0530

    writing to csv from a list

--- write_to_csv.py
+++ write_to_csv.py
@@ -6,12 +6,12 @@ home = os.path.expanduser("~")
 downloads = os.path.join(home, "Downloads")
 path = os.path.join(downloads, 'hdfc_fail.json')
 
-with open(path, 'r') as f:
+with open('icici_fail.json', 'r') as f:
     data = json.load(f)
 
 data = data['filesToProcess']
 
-with open('output2.csv', 'w', newline='') as f:
+with open('icici_triangulation_fail.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['id'])
     # Loop through the list of data and append each item to a new row in the CSV file
