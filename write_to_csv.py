d8803b8720ac0738ed224d7d0e7d4754eeafae89
Randheer kumar <randheer.dakshana15@gmail.com>
2023-04-19 16:15:03 +0530

    changes

--- write_to_csv.py
+++ write_to_csv.py
@@ -6,12 +6,12 @@ home = os.path.expanduser("~")
 downloads = os.path.join(home, "Downloads")
 path = os.path.join(downloads, 'hdfc_fail.json')
 
-with open('icici_pair.json', 'r') as f:
+with open('karur_fail.json', 'r') as f:
     data = json.load(f)
 
 data = data['filesToProcess']
 
-with open('icici_probable_pair.csv', 'w', newline='') as f:
+with open('karur_triangulation_fail.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['id'])
     # Loop through the list of data and append each item to a new row in the CSV file
