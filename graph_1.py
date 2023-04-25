eb6ab3ff03eb8ff52f258b1ea6c20ff30e79443b
Randheer kumar <randheer.dakshana15@gmail.com>
2023-04-25 14:44:19 +0530

    graph learning"

---
+++ graph_1.py
@@ -0,0 +1,17 @@
+n = 6
+g = []
+g2 = {}
+# for i in range(6):
+#     temp = []
+#     for j in range(6):
+#         temp.append(0)
+#     g.append(temp)
+#
+# for i in g:
+#     print(i)
+for i in range(6):
+    g2[i] = []
+
+print(g2)
+for item in g2.items():
+    print(item)
