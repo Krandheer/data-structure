9cea8ab644409e6be98b896d5d97803110e7e988
Randheer kumar <randheer.dakshana15@gmail.com>
2023-05-01 22:19:33 +0530

    bipartite graph code

---
+++ bipartite_graph.py
@@ -0,0 +1,29 @@
+graph = {
+    1: [3],
+    2: [1, 3, 6],
+    3: [2, 4],
+    4: [3, 7, 5],
+    5: [4, 6],
+    6: [2, 5],
+    7: [4, 8],
+    8: [7]
+}
+
+color = []
+for i in range(9):
+    color.append(-1)
+
+
+def dfs(graph, node, col, color):
+    color[node] = col
+
+    for child in graph[node]:
+        if color[child] == -1:
+            if not dfs(graph, child, 1 - col, color):
+                return False
+        elif color[child] == color[node]:
+            return False
+    return True
+
+
+print(dfs(graph, 1, 0, color))
