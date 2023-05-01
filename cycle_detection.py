0ee2b8b3bcfb563c6a05f8f8c2da254e52329065
Randheer kumar <randheer.dakshana15@gmail.com>
2023-05-01 14:55:46 +0530

    cycle detection in graph

---
+++ cycle_detection.py
@@ -0,0 +1,38 @@
+"""
+cycle detection in undirected graph, this same can be done using dfs as well.
+the concept is that when you fall on visited not then it should not parent and if so then there is cycle.
+"""
+
+from collections import deque
+
+ipt = [[1, 2], [1, 3], [2, 3], [2, 4], [4, 5], [5, 1]]
+n = 5
+graph = {}
+visited = {}
+
+for i in range(1, n + 1):
+    graph[i] = []
+    visited[i] = False
+for u, v in ipt:
+    graph[u].append(v)
+    graph[v].append(u)
+
+
+def bfs_cycle(graph, node, visited, par):
+    queue = deque()
+    queue.append((node, par))
+    visited[node] = True
+
+    while queue:
+        node, par = queue.popleft()
+        for child in graph[node]:
+            if not visited[child]:
+                queue.append((child, node))  # Append a tuple (child, node)
+                visited[child] = True
+            else:
+                if child != par:
+                    return True
+    return False
+
+
+print(bfs_cycle(graph, 1, visited, -1))
