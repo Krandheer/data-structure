e2599e340dad425fd06004639ed031176dc7e226
Randheer kumar <randheer.dakshana15@gmail.com>
2023-05-10 22:08:32 +0530

    changes

--- dijkstra_algo_graph.py
+++ dijkstra_algo_graph.py
@@ -1,6 +1,5 @@
 """
-Algo for sssp for directed graph, for undirected graph simple sssp with bfs using level was traversal works,
+Algo for sssp for directed graph, for undirected graph simple sssp with bfs using level order traversal works,
 actually this will work for undirected as well, just use min heap instead of queue and then bfs will do job,
 heap will make sure that you pop min element that minimise the distance.
 """
-
