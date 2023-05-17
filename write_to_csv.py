71a288046414b1495c4805f2f90584d820ddb60f
Randheer kumar <randheer.dakshana15@gmail.com>
2023-05-18 00:13:43 +0530

    some changes

--- write_to_csv.py
+++ write_to_csv.py
@@ -6,12 +6,12 @@ home = os.path.expanduser("~")
 downloads = os.path.join(home, "Downloads")
 path = os.path.join(downloads, 'hdfc_fail.json')
 
-with open('hdfc_auth_fail_4th_may.json', 'r') as f:
+with open('hdfc_auth_fail_28th_feb.json', 'r') as f:
     data = json.load(f)
 
 data = data['filesToProcess']
 
-with open('hdfc_auth_4th_may_analysis.csv', 'w', newline='') as f:
+with open('hdfc_auth_28th_feb_analysis.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['ATMID'])
     # Loop through the list of data and append each item to a new row in the CSV file
