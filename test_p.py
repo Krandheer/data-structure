3167138a0c988dd2a42e94db8895746ed9f92dc2
Randheer kumar <randheer.dakshana15@gmail.com>
2023-05-24 19:02:38 +0530

    changes

--- test_p.py
+++ test_p.py
@@ -1,20 +1,31 @@
-import collections
+# import collections
+#
+# Card = collections.namedtuple('Card', ['rank', 'suit'])
+#
+#
+# class FrenchDeck:
+#     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+#     suits = 'spades diamonds clubs hearts'.split()
+#
+#     def __init__(self):
+#         self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+#
+#     def __len__(self):
+#         return len(self._cards)
+#
+#     def __getitem__(self, position): return self._cards[position]
+#
+#
+# a = FrenchDeck()
+# print(len(a))
 
-Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+def factorial(n, a=1):
+    if n <= 1:
+        return a
+    else:
+        return factorial(n - 1, a * n)
 
 
-class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'spades diamonds clubs hearts'.split()
 
-    def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
-
-    def __len__(self):
-        return len(self._cards)
-
-    def __getitem__(self, position): return self._cards[position]
-
-
-a = FrenchDeck()
-print(len(a))
+print(factorial(5))
