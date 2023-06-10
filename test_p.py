beb90999b17dcf5ff3da16f2c4a8712318a7b225
Randheer kumar <randheer.dakshana15@gmail.com>
2023-06-10 17:26:22 +0530

    global variable concept recap

--- test_p.py
+++ test_p.py
@@ -18,6 +18,22 @@
 #
 # a = FrenchDeck()
 # print(len(a))
+z = 1
+
+
+def check():
+    global z
+    a = z + 1
+    print(a)
+
+
+check()
+print(z)
+
+"""
+a variable defined outside the scope of function can be accessed inside the function but can't be modified without
+ declaring it as global inside the function you trying to modifying it.
+"""
 
 
 def fun(x, y):
@@ -26,5 +42,4 @@ def fun(x, y):
     else:
         return fun(x - 1, x + y)
 
-
-print(fun(5, 2))
+# print(fun(5, 2))
