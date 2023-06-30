e1ac477d1f7dcea3daf68d164d50a00a69591b91
Randheer kumar <randheer.dakshana15@gmail.com>
2023-06-30 18:22:47 +0530

    changes

--- list_1.py
+++ list_1.py
@@ -27,17 +27,3 @@ for i in atmid:
 
 print(len(to_check))
 print(to_check)
-
-temp = [
-        ]
-
-
-"""these 14 ids are missing in new pnb denom mapping provided by writer pnb = ['A1110210', 'A1125610', 'A1178510', 
-'A1191110', 'A1214810', 'A2124210', 'A2187110', 'DC308000', 'N2169400', 'N4585900', 'N6153000', 'N8150500', 
-'N8308000', 'NB600100'] 
-
-confusing_pnb = ['D1015310', 'D1038920', 'D1770400', 'D2585900', 'D3076100', 'D5623800', "D5762600", "D6084600", 
-"D6149400", "D7060700", 'DA386900', ] 
-
-more_confusing = ['10416601', ]
-"""
