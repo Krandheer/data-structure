72f1cc58e3ad479cac36c13a57f914c6fae1cc93
randheer <randheer.dakshana15@gmail.com>
2023-07-27 23:50:41 +0530

    changes

--- move_files.py
+++ move_files.py
@@ -6,13 +6,13 @@ import pandas as pd
 import json
 import shutil
 
-base_dir = os.path.expanduser('~')
+base_dir = os.path.expanduser("~")
 
-downloads = os.path.join(base_dir, 'Downloads')
+downloads = os.path.join(base_dir, "Downloads")
 
 
 def move(src, desti):
-    files_to_move = glob(f'{src}/{id}*')
+    files_to_move = glob(f"{src}/{id}*")
     for temp in files_to_move:
         shutil.copy(temp, desti)
 
@@ -20,68 +20,91 @@ def move(src, desti):
 # here folder is destination
 def move_to_folder(folder):
     for id in atm_ids:
-        if 'S1AC' in id:
-            source = os.path.join(downloads, 'chunk_28_1')
+        if "S1AC" in id:
+            source = os.path.join(downloads, "chunk_28_1")
             move(source, folder)
 
-        elif 'S1AW' in id:
-            source = os.path.join(downloads, 's1aw')
+        elif "S1AW" in id:
+            source = os.path.join(downloads, "s1aw")
             move(source, folder)
 
-        elif 'S1AN' in id:
-            source = os.path.join(downloads, 'chunk_1')
+        elif "S1AN" in id:
+            source = os.path.join(downloads, "chunk_1")
             move(source, folder)
 
 
 def create_json():
-    with open(f'{downloads}/hdfc.json', 'w') as f:
-        json.dump({'hdfc': atm_ids}, f)
+    with open(f"{downloads}/hdfc.json", "w") as f:
+        json.dump({"hdfc": atm_ids}, f)
 
 
 def create_folder_inside_folder():
-    hdfc_reports = os.path.join(downloads, 'hdfc_reports')
+    hdfc_reports = os.path.join(downloads, "hdfc_reports")
     for ids in atm_ids:
-        os.makedirs(f'{hdfc_reports}/{ids}', exist_ok=True)
+        os.makedirs(f"{hdfc_reports}/{ids}", exist_ok=True)
 
 
 def file_inside_folder():
-    hdfc_reports = os.path.join(downloads, '23rd_june_hdfc_auth')
+    hdfc_reports = os.path.join(downloads, "23rd_june_hdfc_auth")
     os.listdir(hdfc_reports)
 
 
 def copy_from_one_folder_to_another():
-    path = os.path.join(downloads, '3rd-july-axis-auth')
+    path = os.path.join(downloads, "3rd-july-axis-auth")
 
-    temp = set()
-    for ids in os.listdir(path):
-        ids = ids.split("_")[0]
-        temp.add(ids)
-    temp = list(temp)
-    temp1 = temp[:100]
-    temp2 = temp[100:]
+    # temp = set()
+    # for ids in os.listdir(path):
+    #     ids = ids.split("_")[0]
+    #     temp.add(ids)
+
+    temp = [
+        "AECN45412",
+        "AECN45436",
+        "APCN01018",
+        "APCN02118",
+        "APCN03109",
+        "APCN19225",
+        "APCN24014",
+        "APCN45449",
+        "APCN50903",
+        "APCN59151",
+        "APCN84905",
+        "APCN85619",
+        "BECN115310",
+        "BECN134205",
+        "BECN216903",
+        "BPCN105320",
+        "BPCN113202",
+        "BPCN129116",
+        "BPCN129603",
+        "BPCN130407",
+        "BPCN138208",
+        "BPCN148106",
+        "BPCN149816",
+        "BPCN153104",
+        "BPCN164202",
+    ]
+    # temp = list(temp)
+    # temp1 = temp[:100]
+    # temp2 = temp[100:]
     print(len(temp))
 
-    dest1 = os.path.join(downloads, '3rd-july-axis-debug1')
-    dest2 = os.path.join(downloads, '3rd-july-axis-debug2')
+    dest1 = os.path.join(downloads, "3rd_july_axis_debug")
+    # dest2 = os.path.join(downloads, "3rd_july_axis_debug")
 
-    for i in temp1:
+    for i in temp:
         paths = glob(f"{path}/{i}*")
         print(paths)
         for j in paths:
             shutil.copy(j, dest1)
-    for i in temp2:
-        paths = glob(f"{path}/{i}*")
-        # print(paths)
-        for j in paths:
-            shutil.copy(j, dest2)
+
 
 copy_from_one_folder_to_another()
 
 
 def another2():
-    path = os.path.join(downloads, 'hdfc_auth_29th_april')
-    path_csv = os.path.join(
-        downloads, "hdfc probable pair correction - Sheet2.csv")
+    path = os.path.join(downloads, "hdfc_auth_29th_april")
+    path_csv = os.path.join(downloads, "hdfc probable pair correction - Sheet2.csv")
     df = pd.read_csv(path_csv)
     csv_temp = []
     for i in df["ATMID"]:
