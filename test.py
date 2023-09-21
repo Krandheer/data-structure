2fba90016cccae8ca35f7f56ed8e1ad0f6dffe0e
randheer <randheer.dakshana15@gmail.com>
2023-09-21 16:28:30 +0530

    dsa revision

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def print_sum(i, sum_n):
     return print_sum(i - 1, sum_n)
 
 
-print(print_sum(5, 0))
+# print(print_sum(5, 0))
 
 
 def print_sum2(i):
@@ -15,4 +15,16 @@ def print_sum2(i):
     return i + print_sum2(i - 1)
 
 
-print(print_sum2(6))
+# print(print_sum2(6))
+
+
+def reverse_array(i, arr):
+    if i >= len(arr) / 2:
+        return
+    arr[i], arr[len(arr) - i - 1] = arr[len(arr) - i - 1], arr[i]
+    reverse_array(i + 1, arr)
+
+
+arr = [1, 2, 4, 3, 5]
+reverse_array(0, arr)
+print(arr)
