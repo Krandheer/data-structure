47daeea3ec3ed56615183fc91e755925fa682954
randheer <randheer.dakshana15@gmail.com>
2023-09-27 20:11:41 +0530

    vertical tree traversal

---
+++ vertical_tree_traversal.py
@@ -0,0 +1,59 @@
+# Function to perform vertical order traversal of a binary tree
+from collections import defaultdict
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def vertical_order_traversal(root):
+    if not root:
+        return []
+
+    # Create a dictionary to store nodes in each column
+    column_map = defaultdict(list)
+
+    # Create a queue to perform BFS
+    queue = deque([(root, 0)])  # (node, column)
+
+    # Perform BFS
+    while queue:
+        node, column = queue.popleft()
+
+        # Add the node to the corresponding column
+        column_map[column].append(node.value)
+
+        # Enqueue left and right children with updated columns
+        if node.left:
+            queue.append((node.left, column - 1))
+        if node.right:
+            queue.append((node.right, column + 1))
+
+    # Sort the columns by their column number
+    sorted_columns = sorted(column_map.items(), key=lambda x: x[0])
+
+    # Extract the values in sorted order
+    result = [values for column, values in sorted_columns]
+
+    # print(result)
+    return result
+
+
+# Example usage:
+# Construct a binary tree
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+# Perform vertical order traversal
+vertical_order = vertical_order_traversal(root)
+for column in vertical_order:
+    print(column)
