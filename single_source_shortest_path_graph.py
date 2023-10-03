2f1a81641b0fa9bb0a7d9af5c21de2b1646312a0
randheer <randheer.dakshana15@gmail.com>
2023-10-03 22:32:44 +0530

    single source shortest path

--- single_source_shortest_path_graph.py
+++ single_source_shortest_path_graph.py
@@ -15,17 +15,37 @@ def bfs(graph, start):
             if child not in visited:
                 visited.add(child)
                 queue.append(child)
-                distance[child] = distance[node]+1
+                distance[child] = distance[node] + 1
     print(f"distance: {distance}")
 
 
-ipt = [[1, 2], [2, 3], [1, 3], [1, 4], [1, 6], [3, 6], [1, 8], [1, 7], [8, 9], [7, 9], [3, 4], [3, 5], ]
+def adjancy_list_rep(graph, node):
+    g = {}
+    for i in range(node):
+        g[i] = []
 
-graph = {}
-for i in range(len(ipt) + 1):
-    graph[i] = []
-for u, v in ipt:
-    graph[u].append(v)
-    graph[v].append(u)
+    for u, v in graph:
+        g[u].append(v)
+        g[v].append(u)
 
+    return g
+
+
+ipt = [
+    [1, 2],
+    [2, 3],
+    [1, 3],
+    [1, 4],
+    [1, 6],
+    [3, 6],
+    [1, 8],
+    [1, 7],
+    [8, 9],
+    [7, 9],
+    [3, 4],
+    [3, 5],
+]
+
+
+graph = adjancy_list_rep(ipt, 10)
 bfs(graph, 1)
