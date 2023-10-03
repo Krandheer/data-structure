8ad48c42d8fc5dfa00cc634ae3482837e5b9535c
randheer <randheer.dakshana15@gmail.com>
2023-10-03 18:56:03 +0530

    dfs modifications

--- dfs.py
+++ dfs.py
@@ -13,7 +13,7 @@ def dfs(graph, start_node, visited=None):
 graph_1 = {"A": ["B", "C"], "B": ["D", "E"], "C": ["F"], "D": [], "E": ["F"], "F": []}
 
 
-dfs(graph_1, "A")
+# dfs(graph_1, "A")
 graph_2 = [
     [1, 2],
     [1, 5],
@@ -24,8 +24,23 @@ graph_2 = [
     [3, 6],
     [4, 5],
     [4, 6],
-    [5],
     [5, 6],
 ]
 
-dfs(graph_2, 0)
+
+def adjancy_list_rep(graph, node):
+    g = {}
+    for i in range(node):
+        g[i] = []
+
+    for u, v in graph:
+        g[u].append(v)
+        g[v].append(u)
+
+    return g
+
+
+graph_2 = adjancy_list_rep(graph_2, 7)
+dfs(graph_2, 1)
+# for key, value in graph_2.items():
+#     print(key, "->", value)
