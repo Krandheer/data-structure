02d3a98d366f6fd565937af03012fee883cb15a5
randheer <randheer.dakshana15@gmail.com>
2023-10-03 18:24:27 +0530

    graph adjancy list representation

--- graph_representation.py
+++ graph_representation.py
@@ -14,9 +14,19 @@ def adjancy_matrix_rep(graph, node):
 
 def adjancy_list_rep(graph, node):
     g = {}
+    for i in range(node):
+        g[i] = []
+
+    for u, v in graph:
+        g[u].append(v)
+
+    return g
 
 
 graph = [[0, 1], [0, 2], [0, 3], [0, 4], [1, 3], [2, 3], [2, 4], [2, 5], [3, 5]]
-graph_1 = adjancy_matrix_rep(graph, 6)
-for i in graph_1:
-    print(i)
+# graph_1 = adjancy_matrix_rep(graph, 6)
+# for i in graph_1:
+#     print(i)
+graph_1 = adjancy_list_rep(graph, 6)
+for key, value in graph_1.items():
+    print(key, "->", value)
