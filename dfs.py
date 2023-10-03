53decabedcd15c826f5635dcf81e63afa69ee718
randheer <randheer.dakshana15@gmail.com>
2023-10-03 18:45:59 +0530

    dfs

--- dfs.py
+++ dfs.py
@@ -10,17 +10,22 @@ def dfs(graph, start_node, visited=None):
             dfs(graph, neighbor, visited)
 
 
-graph_1 = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['F'],
-    'F': []
-}
+graph_1 = {"A": ["B", "C"], "B": ["D", "E"], "C": ["F"], "D": [], "E": ["F"], "F": []}
 
 
-dfs(graph_1, 'A')
-graph_2 = [[1, 2], [3, 4], [5], [], [5], []]
+dfs(graph_1, "A")
+graph_2 = [
+    [1, 2],
+    [1, 5],
+    [2, 3],
+    [2, 4],
+    [2, 5],
+    [3, 4],
+    [3, 6],
+    [4, 5],
+    [4, 6],
+    [5],
+    [5, 6],
+]
 
 dfs(graph_2, 0)
