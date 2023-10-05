d70ab30a15a942c4b636ef0626895b7f92b6f58d
randheer <randheer.dakshana15@gmail.com>
2023-10-05 22:37:13 +0530

    without global timer

--- in_out_time_graph.py
+++ in_out_time_graph.py
@@ -10,25 +10,25 @@ def adjancy_list_rep(graph, node):
     return g
 
 
-def dfs(graph, node, intime, outtime, visited=None):
+def dfs(graph, node, intime, outtime, timer=1, visited=None):
     if not visited:
         visited = set()
-    global timer
+    # global timer
     # print(node)
     visited.add(node)
     intime[node] = timer
     timer += 1
     for child in graph[node]:
         if child not in visited:
-            dfs(graph, child, intime, outtime, visited)
+            intime, outtime, timer = dfs(graph, child, intime, outtime, timer, visited)
     outtime[node] = timer
     timer += 1
-    return intime, outtime
+    return intime, outtime, timer
 
 
 graph = adjancy_list_rep(ipt, 6)
 intime, outime = {}, {}
-timer = 1
+# timer = 1
 # print("graph", graph)
 dfs(graph, 0, intime, outime)
 print(intime, outime)
