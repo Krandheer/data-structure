0d9b3c23364218fe62b48250ffd17ce8a505cdf2
randheer <randheer.dakshana15@gmail.com>
2023-10-06 12:19:04 +0530

    subtree size

--- subtree_size.py
+++ subtree_size.py
@@ -10,16 +10,22 @@ def adjancy_list_rep(graph, node):
     return g
 
 
-def dfs(graph, node, visited=None):
+def dfs(graph, node, distance, visited=None):
     if not visited:
         visited = set()
     visited.add(node)
+    temp = 0
     for child in graph[node]:
         if child not in visited:
-            dfs(graph, child, visited)
+            t_temp, _ = dfs(graph, child, distance, visited)
+            temp = temp + t_temp
+
+    distance[node] = temp + 1
+    return distance[node], distance
 
 
 ipt = [[1, 2], [2, 4], [2, 5], [2, 6], [1, 3], [3, 7], [1, 8], [6, 10], [1, 9]]
 graph = adjancy_list_rep(ipt, 11)
 
-dfs(graph, 1, {})
+_, result = dfs(graph, 1, {})
+print(result)
