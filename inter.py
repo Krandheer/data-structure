9a52bb86e4d15c5a11e328539e4274bdbeb0d5ef
randheer <randheer.dakshana15@gmail.com>
2023-10-07 23:07:15 +0530

    interveiw question for edit minimum distance

--- inter.py
+++ inter.py
@@ -7,6 +7,8 @@ def compare_string(index1, index2, string1, string2):
         return index2 + 1
     if index2 <= 0:
         return index1 + 1
+    if index1 <= 0 and index2 <= 0:
+        return 0
     if string1[index1] == string2[index2]:
         index1 = index1 - 1
         index2 = index2 - 1
