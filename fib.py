99f6a89966161ab6230dc3fc66a418e05e9ce0ce
randheer <randheer.dakshana15@gmail.com>
2023-10-12 00:02:03 +0530

    optimised

--- fib.py
+++ fib.py
@@ -2,19 +2,19 @@ import time
 
 
 def fib1(n):
-    if n == 0 or n == 1:
+    if n <= 1:
         return n
     else:
         return fib1(n - 1) + fib1(n - 2)
 
 
 fib1_time = time.time()
-print(fib1(10))
-print(f'fib1 time = {time.time() - fib1_time}')
+print(fib1(40))
+print(f"fib1 time = {time.time() - fib1_time}")
 
 
 def fib_dp(n, dp):
-    if n == 0 or n == 1:
+    if n <= 1:
         return n
     elif dp[n] != -1:
         return dp[n]
@@ -24,5 +24,5 @@ def fib_dp(n, dp):
 
 
 fib_dp_time = time.time()
-print(fib_dp(10, [-1] * 11))
+print(fib_dp(40, [-1] * 41))
 print(f"fib_dp_time = {time.time() - fib_dp_time}")
