fe87d106abe7125e64bb83b7c3cabdb08f927b51
randheer <randheer.dakshana15@gmail.com>
2023-10-12 16:23:45 +0530

    recurssin added

--- climb_stairs_leetcode.py
+++ climb_stairs_leetcode.py
@@ -1,3 +1,10 @@
+def climb_stair(n):
+    if n == 1 or n == 2:
+        return n
+    else:
+        return climb_stair(n - 1) + climb_stair(n - 2)
+
+
 def climbstairs_leetcode(self, n, dp):
     """
     leetcode dp day 2 question
@@ -8,5 +15,6 @@ def climbstairs_leetcode(self, n, dp):
     elif dp[n] != -1:
         return dp[n]
     else:
-        dp[n] = self.climbstairs_leetcode(n - 1, dp) + self.climbstairs_leetcode(n - 2, dp)
+        dp[n] = self.climbstairs_leetcode(n - 1, dp)
+        +self.climbstairs_leetcode(n - 2, dp)
         return dp[n]
