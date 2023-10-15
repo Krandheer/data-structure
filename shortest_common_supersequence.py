217b4286636d2b48e4367bc7950a14bed8dc3720
randheer <randheer.dakshana15@gmail.com>
2023-10-15 22:47:32 +0530

    shortest common supersequence

---
+++ shortest_common_supersequence.py
@@ -0,0 +1,23 @@
+# return the shortest common supersequence containing both sequences
+def shortest_common_supersequence(s1, s2):
+    def lcs(index1, index2, s1, s2):
+        if index1 < 0 or index2 < 0:
+            return 0
+
+        if s1[index1] == s2[index2]:
+            return 1 + lcs(index1 - 1, index2 - 1, s1, s2)
+        else:
+            return max(
+                lcs(index1 - 1, index2, s1, s2),
+                lcs(index1, index2 - 1, s1, s2),
+            )
+
+    index1 = len(s1) - 1
+    index2 = len(s2) - 1
+    temp = lcs(index1, index2, s1, s2)
+    return len(s1) + len(s2) - temp
+
+
+s1 = "bleed"
+s2 = "blue"
+print(shortest_common_supersequence(s1, s2))
