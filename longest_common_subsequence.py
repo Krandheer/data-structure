583eb46def8c209f0f8ca7c2695d2011b60164c9
randheer <randheer.dakshana15@gmail.com>
2023-10-15 16:05:05 +0530

    reformatted

--- longest_common_subsequence.py
+++ longest_common_subsequence.py
@@ -1,5 +1,5 @@
-s1 = 'adcbc'
-s2 = 'dcadb'
+s1 = "adcbc"
+s2 = "dcadb"
 
 """
 to get the longest common string convert the solution to bottom up, and start tracking from right bottom in table
@@ -17,7 +17,10 @@ def lcs(index1, index2, string1, string2):
     if string1[index1] == string2[index2]:
         return 1 + lcs(index1 - 1, index2 - 1, string1, string2)
     else:
-        return max(lcs(index1 - 1, index2, string1, string2), lcs(index1, index2 - 1, string1, string2))
+        return max(
+            lcs(index1 - 1, index2, string1, string2),
+            lcs(index1, index2 - 1, string1, string2),
+        )
 
 
 print(lcs(4, 4, s1, s2))
@@ -33,8 +36,10 @@ def lcs_dp(index1, index2, string1, string2, dp):
         dp[index1][index2] = 1 + lcs_dp(index1 - 1, index2 - 1, string1, string2, dp)
         return dp[index1][index2]
     else:
-        dp[index1][index2] = max(lcs_dp(index1 - 1, index2, string1, string2, dp),
-                                 lcs_dp(index1, index2 - 1, string1, string2, dp))
+        dp[index1][index2] = max(
+            lcs_dp(index1 - 1, index2, string1, string2, dp),
+            lcs_dp(index1, index2 - 1, string1, string2, dp),
+        )
         return dp[index1][index2]
 
 
@@ -42,4 +47,3 @@ dp = [[-1 for j in range(len(s2))] for i in range(len(s1))]
 print(lcs_dp(len(s1) - 1, len(s2) - 1, s1, s2, dp))
 for i in dp:
     print(i)
-
