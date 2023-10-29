1c0058735fa965c1ef5608e266f7e8e0c0672856
randheer <randheer.dakshana15@gmail.com>
2023-10-29 23:16:31 +0530

    optimsied

--- grid_travel_graph.py
+++ grid_travel_graph.py
@@ -10,8 +10,8 @@ def grid_travel(m, n, i, j, visited):
     if i >= m or j >= n or visited[i][j]:
         return 0
     visited[i][j] = 1
-    down = grid_travel(m, n, i, j + 1, visited)
-    right = grid_travel(m, n, i + 1, j, visited)
+    right = grid_travel(m, n, i, j + 1, visited)
+    down = grid_travel(m, n, i + 1, j, visited)
     visited[i][j] = 0
     return down + right
 
