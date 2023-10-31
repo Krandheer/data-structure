ad5a4d0c603edd2c4f9b4e533403e0ddab22774d
randheer <randheer.dakshana15@gmail.com>
2023-10-31 22:41:11 +0530

    front partitioning for palindrome partitioning

--- palindrome_partitioning.py
+++ palindrome_partitioning.py
@@ -2,3 +2,30 @@
 given a string find the minimum number of partitions so that all partitions are palindrome.
 solve using the concept of fron partitioning.
 """
+import math
+
+# ipt = "bababcbadcede"
+ipt = "ABC"
+
+
+def is_palindrome(i, j, str):
+    while i <= j:
+        if str[i] != str[j]:
+            return False
+        i = i + 1
+        j = j - 1
+    return True
+
+
+def palindrome_partition(i, str):
+    if i >= len(str):
+        return 0
+    min_cost = math.inf
+    for j in range(i, len(str)):
+        if is_palindrome(i, j, str):
+            cost = 1 + palindrome_partition(j + 1, str)
+            total_cost = min(min_cost, cost)
+    return total_cost
+
+
+print(palindrome_partition(0, ipt) - 1)
