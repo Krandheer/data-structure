951388aa35e78d2fd459155582220c400761c55e
randheer <randheer.dakshana15@gmail.com>
2023-11-05 18:26:49 +0530

    dp solution added

--- find_triplet.py
+++ find_triplet.py
@@ -36,7 +36,50 @@ def find_triplet(arr, target, triplet=[], index=0):
 # Example usage
 arr = [1, 2, 3, 4, 5]
 target = 9
-result = find_triplet(arr, target)
+# result = find_triplet(arr, target)
+
+# if result:
+# print("Triplet found:", result)
+# else:
+# print("No triplet found")
+
+
+def find_triplet_memo(arr, target, triplet=[], index=0, memo={}):
+    if len(triplet) == 3 and sum(triplet) == target:
+        return triplet
+    if len(triplet) == 3 or index == len(arr):
+        return None
+
+    # Convert triplet to a tuple to use it as a key in the memoization dictionary
+    triplet_key = tuple(triplet)
+
+    if triplet_key in memo:
+        return memo[triplet_key]
+
+    # Take case
+    triplet.append(arr[index])
+    include_triplet = find_triplet_memo(arr, target, triplet, index + 1, memo)
+
+    # Not take case
+    if include_triplet is None:
+        triplet.pop()
+        exclude_triplet = find_triplet_memo(arr, target, triplet, index + 1, memo)
+
+    if include_triplet:
+        memo[triplet_key] = include_triplet
+        return memo[triplet_key]
+    elif exclude_triplet:
+        memo[triplet_key] = exclude_triplet
+        return memo[triplet_key]
+    else:
+        memo[triplet_key] = None
+        return None
+
+
+# Example usage
+arr = [1, 2, 3, 4, 5, 6, 7]
+target = 10
+result = find_triplet_memo(arr, target)
 
 if result:
     print("Triplet found:", result)
