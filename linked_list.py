193d5ea235b96fe6b978d35131594f0d6d4f6eee
randheer <randheer.dakshana15@gmail.com>
2023-11-06 10:56:05 +0530

    few more methods added

--- linked_list.py
+++ linked_list.py
@@ -31,15 +31,30 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    def delete_at_start(self):
+        if self.head is not None:
+            self.head = self.head.next
+
     def insert_at_end(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
         else:
-            curr = self.head
-            while curr:
-                curr = curr.next
-            curr.next = new_node
+            temp = self.head
+            while temp:
+                temp = temp.next
+            temp.next = new_node
+
+    def delete_at_end(self):
+        if self.head is None:
+            pass
+        elif self.head.next is None:
+            self.head = None
+        else:
+            temp = self.head
+            while temp.next.next is not None:
+                temp = temp.next
+            temp.next = None
 
     def __repr__(self):
         current = self.head
