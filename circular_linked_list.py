bd79f1384bafb61dc75eea1a5bc9e8f0e7317d7b
randheer <randheer.dakshana15@gmail.com>
2023-11-08 17:06:02 +0530

    circular linked list first add implemented

---
+++ circular_linked_list.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class Cll:
+    def __init__(self):
+        self.tail = None
+
+    def is_empty(self):
+        return self.tail is None
+
+    def insert_at_start(self, data):
+        new_node = Node(data)
+        if self.is_empty():
+            new_node.next = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.tail.next
+            self.tail.next = new_node
+
+    def __repr__(self) -> str:
+        if self.is_empty():
+            pass
+        else:
+            head = self.tail.next
+            node = []
+            while head != self.tail:
+                node.append(f"{head.data}")
+                head = head.next
+            node.append(f"{self.tail.data}")
+        return "->".join(node)
+
+
+mylist = Cll()
+mylist.insert_at_start(30)
+mylist.insert_at_start(20)
+mylist.insert_at_start(10)
+print(mylist)
