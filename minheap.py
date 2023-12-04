1367cee8dca963b2517b261f2cf0761955be20ac
randheer <randheer.dakshana15@gmail.com>
2023-12-04 17:35:05 +0530

    bug fixed

--- minheap.py
+++ minheap.py
@@ -7,49 +7,53 @@ import heapq
 class MinHeap:
     def __init__(self):
         self.heap = []
-        self.size = 0
 
-    # to balance the inserted new element in the heap we heapify it
-    def heapify_up(self, i):
-        while i // 2 > 0:
-            if self.heap[i] < self.heap[i // 2]:
-                self.heap[i], self.heap[i // 2] = self.heap[i // 2], self.heap[i]
-            i = i // 2
-
-    # insert the element in the end of the list
-    # then use heapify to maintain the heap property
     def push_heap(self, key):
         self.heap.append(key)
-        self.size = self.size + 1
-        self.heapify_up(self.size - 1)
+        self._heapify_up(len(self.heap) - 1)
 
-    # to find the minimum child of the root
-    # from both available child
-    def min_child(self, i):
-        if 2 * i + 1 > self.size:
-            return 2 * i
-        else:
-            if self.heap[2 * i] < self.heap[2 * i + 1]:
-                return 2 * i
+    def _heapify_up(self, i):
+        while i > 0:
+            parent = (i - 1) // 2
+            if self.heap[i] < self.heap[parent]:
+                self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
+                i = parent
             else:
-                return 2 * i + 1
+                break
 
-    # to del with delete operation we move down
-    # the new root to its logical correct position
-    def heapify_down(self, i):
-        while 2 * i <= self.size:
-            min_child = self.min_child(i)
+    def pop_heap(self):
+        if not self.heap:
+            raise IndexError("heap is empty")
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def _heapify_down(self, i):
+        while 2 * i + 1 < len(self.heap):
+            left_child = 2 * i + 1
+            right_child = 2 * i + 2 if 2 * i + 2 < len(self.heap) else None
+            min_child = (
+                left_child
+                if right_child is None or self.heap[left_child] < self.heap[right_child]
+                else right_child
+            )
+
             if self.heap[i] > self.heap[min_child]:
                 self.heap[i], self.heap[min_child] = self.heap[min_child], self.heap[i]
-            i = min_child
+                i = min_child
+            else:
+                break
 
-    def heap_pop(self):
-        value = self.heap[1]
-        self.heap[0] = self.heap[self.size - 1]
-        self.size = self.size - 1
-        self.heap.pop()
-        self.heapify_down(0)
-        return value
+    def peek(self):
+        if not self.heap:
+            raise IndexError("heap is empty")
+        return self.heap[0]
+
+    def is_empty(self):
+        return not bool(self.heap)
 
 
 class MinHeap2:
@@ -75,16 +79,16 @@ class MinHeap2:
             return IndexError("heap is empty")
 
 
-minheap2 = MinHeap2()
-minheap2.push_heap(5)
-minheap2.push_heap(10)
-minheap2.push_heap(2)
-minheap2.push_heap(8)
-print(minheap2.pop_heap())
+# minheap2 = MinHeap2()
+# minheap2.push_heap(5)
+# minheap2.push_heap(10)
+# minheap2.push_heap(2)
+# minheap2.push_heap(8)
+# print(minheap2.pop_heap())
 
-# minheap = MinHeap()
-# minheap.push_heap(5)
-# minheap.push_heap(10)
-# minheap.push_heap(2)
-# minheap.push_heap(8)
-# print(minheap.heap_pop())
+minheap = MinHeap()
+minheap.push_heap(5)
+minheap.push_heap(10)
+minheap.push_heap(2)
+minheap.push_heap(8)
+print(minheap.pop_heap())
