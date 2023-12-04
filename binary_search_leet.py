127840264f0edf685da08491576f6f117b34d503
randheer <randheer.dakshana15@gmail.com>
2023-12-04 22:45:04 +0530

    one line removed

--- binary_search_leet.py
+++ binary_search_leet.py
@@ -18,7 +18,6 @@ def search(nums, target):
 def searchInsert(nums, target):
     start = 0
     end = len(nums) - 1
-    mid = 0
     while start <= end:
         mid = start + (end - start) // 2
         if nums[mid] == target:
