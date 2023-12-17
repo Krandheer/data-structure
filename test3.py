5390bc3d18f7de895111f026308091bf99aed677
randheer <randheer.dakshana15@gmail.com>
2023-12-17 23:07:29 +0530

    teaching somebody

--- test3.py
+++ test3.py
@@ -1,5 +1,11 @@
-def add(a, b):
-    return a + b
+def good_pairs(num):
+    counter = 0
+    for i in range(len(num)):
+        for j in range(i, len(num)):
+            if num[i] == num[j] and i < j:
+                counter += 1
+    return counter
 
 
-print(add(2,5))
+temp1 = [1, 2, 3, 1, 1, 3]
+print(good_pairs(temp1))
