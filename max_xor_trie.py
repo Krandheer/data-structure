1f5e1f52bdf85a5247df2beac0985debe2e0c804
randheer <randheer.dakshana15@gmail.com>
2023-12-28 23:37:27 +0530

    max xor done

--- max_xor_trie.py
+++ max_xor_trie.py
@@ -1,3 +1,6 @@
+import math
+
+
 class TrieNode:
     def __init__(self):
         self.links = {}
@@ -40,10 +43,30 @@ class Trie:
     #     print("\n".join(result))
 
 
+def get_max_xor(nums):
+    tri = Trie()
+    for num in nums:
+        tri.insert(num)
+    max_xor = -math.inf
+    for num in nums:
+        node = tri.root
+        current_xor = 0
+        for i in range(31, -1, -1):
+            bit = (num >> i) & 1
+            if (1 - bit) in node.links:
+                current_xor += 1 << i
+                node = node.links[1 - bit]
+            else:
+                node = node.links[bit]
+        max_xor = max(max_xor, current_xor)
+    return max_xor
+
+
 t = Trie()
 numbers = [3, 10, 5, 25, 2, 8]
-for num in numbers:
-    t.insert(num)
+# for num in numbers:
+#     t.insert(num)
 # t.insert(4)
-print(t.__repr__())
+# print(t.__repr__())
 # t.prprint()
+print(get_max_xor(numbers))
