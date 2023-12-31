4d08e4397b9b8881d88823852af1d5199fea6322
randheer <randheer.dakshana15@gmail.com>
2023-12-31 11:00:09 +0530

    just exploring the python abstract class and method

--- test3.py
+++ test3.py
@@ -6,7 +6,6 @@
 
 from collections import Counter
 import asyncio
-import collections
 
 
 def get_missing(N, arr):
@@ -66,5 +65,34 @@ def remove_vowels(str1):
     return a
 
 
-str1 = "sunitaalagi"
-print(remove_vowels(str1))
+# str1 = "sunitaalagi"
+# print(remove_vowels(str1))
+
+
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+    def perimeter(self):
+        return 4 * self.side
+
+
+sq = Square(2)
+print(sq.area())
+print(sq.perimeter())
