9bb51ffa417c7e414c14c2f1a775295822f1578f
randheer <randheer.dakshana15@gmail.com>
2024-01-17 10:08:20 +0530

    optimised the code

--- test.py
+++ test.py
@@ -39,15 +39,15 @@ def left_rotate_1(arr):
     return arr
 
 
-# arr = [1, 2, 4, 3, 5]
-# print(left_rotate_1(arr))
+arr = [1, 2, 4, 3, 5]
+print(left_rotate_1(arr))
 
 
 def shift_zero_to_end(arr):
     j = -1
     # find the first zero element index
-    for i in range(len(arr)):
-        if arr[i] == 0:
+    for i in arr:
+        if i == 0:
             j = i
             break
     if j == -1:
@@ -60,8 +60,8 @@ def shift_zero_to_end(arr):
     return arr
 
 
-temp = [1, 0, 2, 3, 2, 0, 0, 4, 5, 1]
-print(shift_zero_to_end(temp))
+#temp = [1, 0, 2, 3, 2, 0, 0, 4, 5, 1]
+#print(shift_zero_to_end(temp))
 
 # two pointers method can be used for union and intersection of two sorted array as well.
 # prefix sum, two pointer method can be used for k-sum as well.
