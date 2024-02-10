b1e6d2f005d344f25dc3ac97f7389dcdbb75af62
randheer <randheer.dakshana15@gmail.com>
2024-02-10 21:24:01 +0530

    pattern match oa solved

--- pattern_match.py
+++ pattern_match.py
@@ -3,6 +3,11 @@ def get_match_index(string, pat):
     # i think take or not take will work here
     # and i feel i need to do dsa question or watch DSA related vidoes everyday
     # for a hour atleast to stay warmed up all time
+    len_str = len(string)
+    len_pat = len(pat)
+    for i in range(len_str - len_pat + 1):
+        if string[i : i + len_pat] == pat:
+            result.append(i)
     return result
 
 
