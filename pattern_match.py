3947568454a008d3875a9ba06e5415b9f3a9da1e
randheer <randheer.dakshana15@gmail.com>
2024-02-10 21:01:13 +0530

    OA

---
+++ pattern_match.py
@@ -0,0 +1,12 @@
+def get_match_index(string, pat):
+    result = []
+    # i think take or not take will work here
+    # and i feel i need to do dsa question or watch DSA related vidoes everyday
+    # for a hour atleast to stay warmed up all time
+    return result
+
+
+STRING = "abababad"
+PATTERN = "aba"
+
+print(get_match_index(STRING, PATTERN))
