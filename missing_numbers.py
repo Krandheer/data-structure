4a8cfb5731841b63b0b6bbda2212c1a88cf464bb
randheer <randheer.dakshana15@gmail.com>
2024-02-14 11:08:04 +0530

    testing few things

--- missing_numbers.py
+++ missing_numbers.py
@@ -16,3 +16,14 @@ def missing_number(nums):
 
     n = len(nums)
     return (n * (n + 1)) // 2 - sum(nums)
+
+
+a = [1, 2, 3, [3, 5, 6], 9]
+for i in a:
+    # if type(i)==int:
+    #     print(i)
+    if isinstance(i, int):
+        print(i)
+    else:
+        for j in i:
+            print(j)
