c0ff859d3102ac8a2436422b238cc1897958c4c9
randheer <randheer.dakshana15@gmail.com>
2024-03-17 12:18:48 +0530

    stiver dsa sheet 4th problem

---
+++ kadanes'_algo_s4.py
@@ -0,0 +1,24 @@
+from typing import *
+
+
+def maxSubArray(nums: List[int]) -> int:
+    """
+    dsa -> s4, day2
+    find the subarray with maximum sum
+    """
+    if max(nums) < 0:
+        return max(nums)
+    maxi = 0
+    temp = 0
+    for i in nums:
+        if i >= 0 or temp + i >= 0:
+            temp += i
+            maxi = max(maxi, temp)
+        else:
+            temp = 0
+    return maxi
+
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# nums = [-1, -2]
+print(maxSubArray(nums))
