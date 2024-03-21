468c3ae742b51ae82d322879f8e3923b689ed9b3
randheer <randheer.dakshana15@gmail.com>
2024-03-21 22:25:59 +0530

    merge interval striver sheet

--- merge_intervals_s8.py
+++ merge_intervals_s8.py
@@ -5,14 +5,24 @@ def merge(intervals: List[List[int]]) -> List[List[int]]:
     """
     https://leetcode.com/problems/merge-intervals/editorial/
     """
-    if len(intervals) == 1:
+    if len(intervals) <= 1:
         return intervals
-    temp = []
-    for i in range(len(intervals) - 1):
-        x, y = intervals[i]
-        u, v = intervals[i + 1]
-        if x <= u <= y and v >= y:
-            temp.append([x, v])
+    intervals = sorted(intervals)
+    temp = [intervals[0]]
+    for i in range(1, len(intervals)):
+        x, y = temp.pop()
+        u, v = intervals[i]
+        if x <= u <= y:
+            if v >= y:
+                temp.append([x, v])
+            else:
+                temp.append([x, y])
         else:
+            temp.append([x, y])
             temp.append([u, v])
     return temp
+
+
+# interval = [[1, 3], [2, 6], [8, 10], [15, 18]]
+interval = [[1, 4], [0, 2], [3, 5]]
+print(merge(interval))
