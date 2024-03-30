b8d9ef9f14959ffd5895abf89635b7f642022688
randheer <randheer.dakshana15@gmail.com>
2024-03-30 17:48:39 +0530

    listified the solution

--- ways_to_climb_stair.py
+++ ways_to_climb_stair.py
@@ -1,16 +1,17 @@
 def ways_to_climb_stairs(p, target, ans):
     if target == 0:
-        ans.append(p)
+        ans.append(list(p))
         return
     ways_to_climb_stairs(p + str(1), target - 1, ans)
     if target >= 2:
         ways_to_climb_stairs(p + str(2), target - 2, ans)
     if target >= 3:
         ways_to_climb_stairs(p + str(3), target - 3, ans)
-    return len(ans)
+    # return len(ans)
+    return ans
 
 
-# print(ways_to_climb_stairs("", 10, []))
+print(ways_to_climb_stairs("", 10, []))
 
 
 def way_dp(target, dp):
@@ -21,8 +22,10 @@ def way_dp(target, dp):
     elif dp[target] != -1:
         return dp[target]
     else:
-        dp[target] = way_dp(target - 1, dp) + way_dp(target - 2, dp) + way_dp(target - 3, dp)
+        dp[target] = (
+            way_dp(target - 1, dp) + way_dp(target - 2, dp) + way_dp(target - 3, dp)
+        )
         return dp[target]
 
 
-print(way_dp(10, [-1] * 11))
+# print(way_dp(10, [-1] * 11))
