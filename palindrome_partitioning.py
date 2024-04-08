ce8cd0c2e59d92d771d48c5327e9de12da7d8db2
randheer <randheer.dakshana15@gmail.com>
2024-04-09 02:30:01 +0530

    revision

--- palindrome_partitioning.py
+++ palindrome_partitioning.py
@@ -6,7 +6,7 @@ solve using the concept of palindrom partitioning.
 import math
 
 # ipt = "bababcbadcede"
-ipt = "ABC"
+ipt = "aabb"
 
 
 def is_palindrome(i, j, str1):
@@ -22,12 +22,11 @@ def palindrome_partition(i, str1):
     if i >= len(str1):
         return 0
     min_cost = math.inf
-    total_cost = 0
     for j in range(i, len(str1)):
         if is_palindrome(i, j, str1):
             cost = 1 + palindrome_partition(j + 1, str1)
-            total_cost = min(min_cost, cost)
-    return total_cost
+            min_cost = min(min_cost, cost)
+    return min_cost
 
 
 print(palindrome_partition(0, ipt) - 1)
