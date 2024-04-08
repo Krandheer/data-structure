4a883a86990ea5e41e31edac6b57def488c44884
randheer <randheer.dakshana15@gmail.com>
2024-04-08 21:48:24 +0530

    updated

--- palindrome_partitioning.py
+++ palindrome_partitioning.py
@@ -1,29 +1,31 @@
 """
 given a string find the minimum number of partitions so that all partitions are palindrome.
-solve using the concept of fron partitioning.
+solve using the concept of palindrom partitioning.
 """
+
 import math
 
 # ipt = "bababcbadcede"
 ipt = "ABC"
 
 
-def is_palindrome(i, j, str):
+def is_palindrome(i, j, str1):
     while i <= j:
-        if str[i] != str[j]:
+        if str1[i] != str1[j]:
             return False
         i = i + 1
         j = j - 1
     return True
 
 
-def palindrome_partition(i, str):
-    if i >= len(str):
+def palindrome_partition(i, str1):
+    if i >= len(str1):
         return 0
     min_cost = math.inf
-    for j in range(i, len(str)):
-        if is_palindrome(i, j, str):
-            cost = 1 + palindrome_partition(j + 1, str)
+    total_cost = 0
+    for j in range(i, len(str1)):
+        if is_palindrome(i, j, str1):
+            cost = 1 + palindrome_partition(j + 1, str1)
             total_cost = min(min_cost, cost)
     return total_cost
 
