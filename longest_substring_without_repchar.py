a1e317aa9a401e9bf2bf65400cc58cacfe4e2345
randheer <randheer.dakshana15@gmail.com>
2024-04-13 10:51:22 +0530

    optimised

--- longest_substring_without_repchar.py
+++ longest_substring_without_repchar.py
@@ -4,19 +4,16 @@ def longest_substring_without_repeating_char(s: str) -> int:
     """
     check = set()
     l, r = 0, 0
-    total = 0
+    max_len = r - l + 1
     while l <= r <= len(s) - 1:
         if s[r] not in check:
-            total = max(total, r - l + 1)
             check.add(s[r])
+            max_len = max(max_len, r - l + 1)
             r += 1
         else:
             check.remove(s[r])
             l += 1
-            total = max(total, r - l)
-    if r == len(s):
-        total = max(total, r - l)
-    return total
+    return max_len
 
 
 sr = ["au", "abcabcbb", "tmmzuxt"]
