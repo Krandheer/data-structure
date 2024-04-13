fe2224724574248d79eec502146454338798ca4a
randheer <randheer.dakshana15@gmail.com>
2024-04-13 11:13:02 +0530

    modified

--- longest_substring_without_repchar.py
+++ longest_substring_without_repchar.py
@@ -2,20 +2,24 @@ def longest_substring_without_repeating_char(s: str) -> int:
     """
     striver sheet 24th problem
     """
-    check = set()
+    check = {}
     l, r = 0, 0
     max_len = r - l + 1
     while l <= r <= len(s) - 1:
         if s[r] not in check:
-            check.add(s[r])
+            check[s[r]] = r
             max_len = max(max_len, r - l + 1)
             r += 1
         else:
-            check.remove(s[r])
-            l += 1
+            p = check[s[r]]
+            if l <= p:
+                l = p + 1
+            check.pop(s[r])
     return max_len
 
 
-sr = ["au", "abcabcbb", "tmmzuxt"]
+sr = ["au", "abcabcbb", "tmmzux"]
 for st in sr:
     print(longest_substring_without_repeating_char(st))
+# st = "tmmzuxt"
+# print(longest_substring_without_repeating_char(st))
