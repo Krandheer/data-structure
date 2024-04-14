d00a46a299ba281405edd853466db53a3f59a489
randheer <randheer.dakshana15@gmail.com>
2024-04-14 13:09:29 +0530

    modified

--- unique_path_with_obstacles.py
+++ unique_path_with_obstacles.py
@@ -2,18 +2,18 @@ from typing import List
 
 
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        n = len(obstacleGrid[0])
-        m = len(obstacleGrid)
-        dp = [[-1 for j in range(n)] for i in range(m)]
-        return self.helper(0, 0, obstacleGrid, dp)
+    def uniquePathsWithObstacles(self, obstacle_grid: List[List[int]]) -> int:
+        n = len(obstacle_grid[0])
+        m = len(obstacle_grid)
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        return self.helper(0, 0, obstacle_grid, dp)
 
-    def helper(self, i, j, obstacleGrid, dp):
-        n = len(obstacleGrid[0])
-        m = len(obstacleGrid)
-        if obstacleGrid[m - 1][n - 1] == 1:
+    def helper(self, i, j, obstacle_grid, dp):
+        n = len(obstacle_grid[0])
+        m = len(obstacle_grid)
+        if obstacle_grid[m - 1][n - 1] == 1:
             return 0
-        if i == 0 and j == 0 and obstacleGrid[i][j] == 1:
+        if i == 0 and j == 0 and obstacle_grid[i][j] == 1:
             return 0
         if i == m - 1 and j == n - 1:
             return 1
@@ -22,10 +22,10 @@ class Solution:
         if dp[i][j] != -1:
             return dp[i][j]
         down = left = 0
-        if i < m - 1 and obstacleGrid[i + 1][j] != 1:
-            down = self.helper(i + 1, j, obstacleGrid, dp)
-        if j < n - 1 and obstacleGrid[i][j + 1] != 1:
-            left = self.helper(i, j + 1, obstacleGrid, dp)
+        if i < m - 1 and obstacle_grid[i + 1][j] != 1:
+            down = self.helper(i + 1, j, obstacle_grid, dp)
+        if j < n - 1 and obstacle_grid[i][j + 1] != 1:
+            left = self.helper(i, j + 1, obstacle_grid, dp)
         dp[i][j] = down + left
         return dp[i][j]
 
