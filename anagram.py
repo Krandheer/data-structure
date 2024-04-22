39ea7454c4fe4a5fc6b0934d837b06356d20c2dc
randheer <randheer.dakshana15@gmail.com>
2024-04-22 20:44:44 +0530

    modified

--- anagram.py
+++ anagram.py
@@ -1,19 +1,17 @@
 from collections import Counter
 
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+def isAnagram(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    s_dict = Counter(s)
+    t_dict = Counter(t)
+    for key, val in s_dict.items():
+        if key not in t_dict or t_dict[key] != val:
             return False
-        s_dict = Counter(s)
-        t_dict = Counter(t)
-        for key, val in s_dict.items():
-            if key not in t_dict or t_dict[key] != val:
-                return False
-        return True
+    return True
 
 
-sol = Solution()
 s1 = "a"
 t1 = "ab"
-print(sol.isAnagram(s1, t1))
+print(isAnagram(s1, t1))
