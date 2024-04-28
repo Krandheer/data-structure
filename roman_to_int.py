7b7ff6768a8ce722ed896c56b80e6c626f198acf
randheer <randheer.dakshana15@gmail.com>
2024-04-28 13:59:08 +0530

    12 and 13 done

--- roman_to_int.py
+++ roman_to_int.py
@@ -12,7 +12,30 @@ class Solution:
                 ans += d_map[s[i]]
         return ans
 
+    def intToRoman(self, num: int) -> str:
+        map_d = {
+            1: "I",
+            5: "V",
+            10: "X",
+            50: "L",
+            100: "C",
+            500: "D",
+            1000: "M",
+            4: "IV",
+            9: "IX",
+            40: "XL",
+            90: "XC",
+            400: "CD",
+            900: "CM",
+        }
+        ans = ""
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            while n <= num:
+                ans += map_d[n]
+                num -= n
+        return ans
+
 
 sol = Solution()
-ans = sol.romanToInt("III")
+ans = sol.intToRoman(58)
 print(ans)
