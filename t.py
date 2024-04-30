b52ecdc3f3fe06d8762f1406f2f061e5ffb53b82
randheer <randheer.dakshana15@gmail.com>
2024-04-30 18:14:56 +0530

    return using list

--- t.py
+++ t.py
@@ -12,7 +12,28 @@ def linear_search(arr, i, target):
     return linear_search(arr, i + 1, target)
 
 
-arr = [1, 2, 4, 11, 9, 12]
+def linear_search2(arr, i, target):
+    ans = []
+    if i == len(arr):
+        return ans
+    if arr[i] == target:
+        ans.append(i)
+    temp = linear_search2(arr, i + 1, target)
+    ans += temp
+    return ans
+
+
+def linear_search3(arr, i, target, ans):
+    if i == len(arr):
+        return ans
+    if arr[i] == target:
+        ans.append(i)
+    return linear_search3(arr, i + 1, target, ans)
+
+
+arr = [1, 2, 4, 4, 11, 9, 12]
 # ans = check_sorted(arr, 1)
-ans = linear_search(arr, 0, 11)
+# ans = linear_search(arr, 0, 11)s
+# ans = linear_search2(arr, 0, 4)
+ans = linear_search3(arr, 0, 4, [])
 print(ans)
