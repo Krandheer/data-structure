453c2d01bd8ebc92d37026e03ed195b248e70914
randheer <randheer.dakshana15@gmail.com>
2024-04-30 18:02:37 +0530

    linear search

--- t.py
+++ t.py
@@ -4,6 +4,15 @@ def check_sorted(arr, i):
     return arr[i] >= arr[i - 1] and check_sorted(arr, i + 1)
 
 
+def linear_search(arr, i, target):
+    if i == len(arr):
+        return -1
+    if arr[i] == target:
+        return i
+    return linear_search(arr, i + 1, target)
+
+
 arr = [1, 2, 4, 11, 9, 12]
-ans = check_sorted(arr, 1)
+# ans = check_sorted(arr, 1)
+ans = linear_search(arr, 0, 11)
 print(ans)
