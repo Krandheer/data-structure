24bbd1a551c386414ca29d6adbad9df0aaf6e367
randheer <randheer.dakshana15@gmail.com>
2024-05-08 10:26:37 +0530

    added comment

--- construct_bt_from_pre_ino_traversal.py
+++ construct_bt_from_pre_ino_traversal.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
 
+# construct binary tree from preorder and indorder traversal
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
