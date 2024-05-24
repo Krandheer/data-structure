23d46b04e1d7b0e484d6d0569e5574e75bfe5627
randheer <randheer.dakshana15@gmail.com>
2024-05-24 19:06:31 +0530

    dp added

--- palindrome_partitioning.py
+++ palindrome_partitioning.py
@@ -21,6 +21,20 @@ def palindrome_partition(i, str1):
     return min_cost
 
 
+def palindrome_partition_dp(i, str1, dp):
+    if i >= len(str1):
+        return 0
+    min_cost = math.inf
+    if dp[i] != -1:
+        return dp[i]
+    for j in range(i, len(str1)):
+        if is_palindrome(i, j, str1):
+            cost = 1 + palindrome_partition(j + 1, str1)
+            min_cost = min(min_cost, cost)
+            dp[i] = min_cost
+    return dp[i]
+
+
 def palidrome_partition2(index, s, ans, path):
     if index >= len(s):
         ans.append(path[:])
@@ -34,7 +48,9 @@ def palidrome_partition2(index, s, ans, path):
     return ans
 
 
-ipt = "aabb"
-# ipt = "bababcbadcede"
-print(palindrome_partition(0, ipt) - 1)
-print(palidrome_partition2(0, ipt, [], []))
+# ipt = "aabb"
+ipt = "bababcbadcede"
+dp = [-1 for i in range(len(ipt))]
+print(palindrome_partition_dp(0, ipt, dp))
+print(palindrome_partition(0, ipt))
+# print(palidrome_partition2(0, ipt, [], []))
