b513547fa67117d77857372b0ae42f5ff21703f8
randheer <randheer.dakshana15@gmail.com>
2024-11-14 23:53:02 +0530

    at each step print the state for better visualisation of algo

--- longest_substring_without_repchar.py
+++ longest_substring_without_repchar.py
@@ -10,16 +10,20 @@ def longest_substring_without_repeating_char(s: str) -> int:
             check[s[r]] = r
             max_len = max(max_len, r - l + 1)
             r += 1
+            print(check)
         else:
             p = check[s[r]]
             if l <= p:
                 l = p + 1
+            print("check from else:", end = " ")
             check.pop(s[r])
+            print(check)
     return max_len
 
 
 sr = ["au", "abcabcbb", "tmmzux"]
 for st in sr:
+    print(st)
     print(longest_substring_without_repeating_char(st))
 # st = "tmmzuxt"
 # print(longest_substring_without_repeating_char(st))
