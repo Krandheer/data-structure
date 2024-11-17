8692ac818eeed891bbc8a2b78d2e56d285ed060a
randheer <randheer.dakshana15@gmail.com>
2024-11-17 16:36:44 +0530

    this passes 65% of cases

--- shortest_subarray_with_sumk.py
+++ shortest_subarray_with_sumk.py
@@ -2,24 +2,26 @@ from typing import List
 
 
 def shortest_subarray(nums: List[int], k: int) -> int:
-    i, j = 0, 0
-    if nums[0]==k:
-        return 1
-
-    smallest_len = len(nums)+1
+    i = 0
+    j = 0
+    smallest_len = len(nums) + 1
     temp = 0
+    n = len(nums)
 
-    while j < len(nums):
-        if temp <= k:
-            temp+=nums[j]
-            j+=1
-        if temp >= k:
-            temp-=nums[i]
-            smallest_len=min(smallest_len, j-i)
-            i+=1
+    while j < n:
+        temp += nums[j]
 
-    if smallest_len>len(nums):
-        return -1
-    return smallest_len
+        while i <= j and temp >= k:
+            smallest_len = min(smallest_len, j - i + 1)
+            temp -= nums[i]
+            i += 1
+
+        j += 1
+
+        if temp <= 0:
+            temp = 0
+            i = j
+
+    return smallest_len if smallest_len <= n else -1
 
 print(shortest_subarray([1,2], 4))
