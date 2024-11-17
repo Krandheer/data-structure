b5c9c0954bc44043b046e1806526198fd2686a3e
randheer <randheer.dakshana15@gmail.com>
2024-11-17 17:42:51 +0530

    leetcode 206

---
+++ minimum_size_subarry_sum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def min_sub_array_len(target: int, nums: List[int]) -> int:
+    i, j = 0, 0
+    temp=0
+    minimal_len = len(nums)+1
+    while j < len(nums):
+        temp+=nums[j]
+        while temp>=target:
+            minimal_len = min(minimal_len, j-i+1)
+            temp-=nums[i]
+            i+=1
+        j+=1
+    return minimal_len if minimal_len<=len(nums) else 0
+
+print(min_sub_array_len(15, [1,2,3,4,5]))
