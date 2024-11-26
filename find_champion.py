e7fe587686e68b8d240aee967c903f185c98643f
randheer <randheer.dakshana15@gmail.com>
2024-11-26 10:58:22 +0530

    leetcode daily challenge

---
+++ find_champion.py
@@ -0,0 +1,15 @@
+def find_champion(self, n: int, edges: List[List[int]]) -> int:
+    """leetcode 2924
+    """
+    incoming = [0] * n
+    for _, d in edges:
+        incoming[d] += 1
+
+    max_winners = []
+    for i, v in enumerate(incoming):
+        if v == 0:
+            max_winners.append(i)
+
+    if len(max_winners) > 1:
+        return -1
+    return max_winners[0]
