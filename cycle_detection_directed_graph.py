740d5ce931d29957dd5b89d353f419472c27fc90
randheer <randheer.dakshana15@gmail.com>
2024-11-30 01:02:16 +0530

    added comment

--- cycle_detection_directed_graph.py
+++ cycle_detection_directed_graph.py
@@ -1,4 +1,8 @@
 def dfs_cycle(graph, node, visited, pathvisited, par):
+    """
+    simple as undirected graph but here we need to check whether the current
+    node is already present in the path or not. For that we are using pathvisited map
+    """
     visited[node] = True
     pathvisited[node] = True
     for child in graph[node]:
@@ -25,4 +29,3 @@ for i in graph:
     if not visited[i]:
         if dfs_cycle(graph, i, visited, pathvisited, -1):
             print(True)
-
