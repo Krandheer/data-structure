0cefc731d4687ed1e3a86097eaf6d6db71116d40
randheer <randheer.dakshana15@gmail.com>
2024-12-04 01:42:33 +0530

    graph revision done

--- strongly_connected_component.py
+++ strongly_connected_component.py
@@ -1,6 +1,6 @@
 """
 uses kosaraju's algorithm to find strongly connected components in a graph.
-basically store all nodes in stack to get some oreder for traversal.
+basically store all nodes in stack to get some order for traversal.
 then reverse the edges and do dfs on each node.
 number of times we call dfs is number of SCCs.
 """
