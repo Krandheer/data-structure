ab2cb92b9b3c63729f8344f841aa92ae8215d33a
randheer <randheer.dakshana15@gmail.com>
2024-12-10 10:55:37 +0530

    leetcode daily challenge

---
+++ longest_special_substring.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+
+def maximum_length(self, s: str) -> int:
+    count = defaultdict(int)
+    for start in range(len(s)):
+        temp = ""
+        for end in range(start, len(s)):
+            if temp == "" or temp[-1] == s[end]:
+                temp += s[end]
+                count[temp] += 1
+            else:
+                break
+    max_len = -1
+    for k, v in count.items():
+        if v >= 3:
+            max_len = max(max_len, len(k))
+    return max_len
