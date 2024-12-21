8a57a9ff3ca770602f235ff10008c54b7a35911f
randheer <randheer.dakshana15@gmail.com>
2024-12-21 19:10:26 +0530

    code refactor

--- house_robber2.py
+++ house_robber2.py
@@ -1,26 +1,23 @@
-class Solution:
-    """leet code dp problem"""
+def rob(nums) -> int:
+    n = len(nums)
 
-    def rob(self, nums) -> int:
-        N = len(nums)
+    if n <= 1:
+        return sum(nums)
 
-        if N <= 1:
-            return sum(nums)
+    if n == 2:
+        return max(nums[0], nums[1])
 
-        if N == 2:
-            return max(nums[0], nums[1])
+    dp1, nums1 = [0] * (n - 1), nums[: n - 1]
+    dp2, nums2 = [0] * (n - 1), nums[1:]
 
-        dp1, nums1 = [0] * (N - 1), nums[:N - 1]
-        dp2, nums2 = [0] * (N - 1), nums[1:]
+    # base cases
+    dp1[0], dp1[1] = nums1[0], max(nums1[0], nums1[1])
+    dp2[0], dp2[1] = nums2[0], max(nums2[0], nums2[1])
 
-        # base cases
-        dp1[0], dp1[1] = nums1[0], max(nums1[0], nums1[1])
-        dp2[0], dp2[1] = nums2[0], max(nums2[0], nums2[1])
+    for i in range(2, n - 1):
+        dp1[i] = max(dp1[i - 1], nums1[i] + dp1[i - 2])
 
-        for i in range(2, N - 1):
-            dp1[i] = max(dp1[i - 1], nums1[i] + dp1[i - 2])
+    for i in range(2, n - 1):
+        dp2[i] = max(dp2[i - 1], nums2[i] + dp2[i - 2])
 
-        for i in range(2, N - 1):
-            dp2[i] = max(dp2[i - 1], nums2[i] + dp2[i - 2])
-
-        return max(dp1[N - 2], dp2[N - 2])
+    return max(dp1[n - 2], dp2[n - 2])
