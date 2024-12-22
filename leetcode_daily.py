c03968200d4916d2660c06c271928c8cb7d5d51d
randheer <randheer.dakshana15@gmail.com>
2024-12-22 17:04:09 +0530

    optimised code

--- leetcode_daily.py
+++ leetcode_daily.py
@@ -279,16 +279,22 @@ def leftmost_building_queries(
 ) -> List[int]:
     # [1,2,1], [0,2]
     ans = [-1] * len(queries)
+    groups = defaultdict(list)
     for i, q in enumerate(queries):
         l, r = sorted(q)
         if l == r or heights[r] > heights[l]:
             ans[i] = r
-            continue
+        else:
+            maxi = max(heights[l], heights[r])
+            groups[r].append((maxi, i))
 
-        for j in range(r + 1, len(heights)):
-            if heights[j] > max(heights[l], heights[r]):
-                ans[i] = j
-                break
+    min_heap = []
+    for i, h in enumerate(heights):
+        for q_h, q_i in groups[i]:
+            heapq.heappush(min_heap, (q_h, q_i))
+        while min_heap and h > min_heap[0][0]:
+            q_h, q_i = heapq.heappop(min_heap)
+            ans[q_i]=i
 
     return ans
 
