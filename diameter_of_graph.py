b42e0963069d4bfef9a7583cf4b9b9364c36de99
randheer <randheer.dakshana15@gmail.com>
2024-12-24 11:38:56 +0530

    code refactor

--- diameter_of_graph.py
+++ diameter_of_graph.py
@@ -1,5 +1,5 @@
 def adjancy_list_rep(graph, node):
-    """adjacency list represenation of graph"""
+    """adjacency list representation of graph"""
     g = {}
     for j in range(1, node):
         g[j] = []
@@ -26,13 +26,13 @@ def dfs(graph, node, visited=None):
 
 ipt = [[1, 2], [2, 4], [2, 5], [2, 6], [1, 3], [3, 7], [1, 8], [6, 10], [1, 9]]
 graph = adjancy_list_rep(ipt, 11)
-# reuslt = dfs(graph_1, 3, {})
-# print(reuslt)
+# result = dfs(graph_1, 3, {})
+# print(result)
 
 # find the diameter by iterating on all node.
 max_r = 0
 for i in range(1, 11):
-    result = dfs(graph, i, {})
+    result = dfs(graph, i, set())
     if result > max_r:
         max_r = result
 print(max_r)
