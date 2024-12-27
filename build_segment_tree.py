10a5493df0a2bd253d5a4bd8dcca4aa7e0be5f64
randheer <randheer.dakshana15@gmail.com>
2024-12-28 00:35:20 +0530

    segment tree

--- build_segment_tree.py
+++ build_segment_tree.py
@@ -8,6 +8,7 @@ def build_segment_tree(arr, segmenttree, start, end, index):
     segmenttree[index] = segmenttree[2 * index + 1] + segmenttree[2 * index + 2]
 
 
-arr = [1,2,3,4]
+arr = [1, 2, 3, 4]
 segmenttree = [0] * (2 * len(arr))
 build_segment_tree(arr, segmenttree, 0, len(arr) - 1, 0)
+print(segmenttree)
