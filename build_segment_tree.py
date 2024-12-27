9ea73f1ce1384ee316c3f0c2ba39c570729438e0
randheer <randheer.dakshana15@gmail.com>
2024-12-28 00:24:07 +0530

    deleted venv

---
+++ build_segment_tree.py
@@ -0,0 +1,13 @@
+def build_segment_tree(arr, segmenttree, start, end, index):
+    if start == end:
+        segmenttree[index] = arr[start]
+        return
+    mid = (start + end) // 2
+    build_segment_tree(arr, segmenttree, start, mid, 2 * index + 1)
+    build_segment_tree(arr, segmenttree, mid + 1, end, 2 * index + 2)
+    segmenttree[index] = segmenttree[2 * index + 1] + segmenttree[2 * index + 2]
+
+
+arr = [1,2,3,4]
+segmenttree = [0] * (2 * len(arr))
+build_segment_tree(arr, segmenttree, 0, len(arr) - 1, 0)
