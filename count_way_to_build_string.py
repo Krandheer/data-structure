0c1fbed0ad0d8fba355cd873cf5bc9cd129c4f95
randheer <randheer.dakshana15@gmail.com>
2024-12-30 11:42:07 +0530

    need to memoize this now

--- count_way_to_build_string.py
+++ count_way_to_build_string.py
@@ -1,12 +1,19 @@
-def helper(low, high, num_zero, num_one, ans):
-    if low <= len(ans) <= high:
+def helper(limit, num_zero, num_one, ans):
+    if len(ans) == limit:
         return 1
-    if len(ans) > high:
+    if len(ans) > limit:
         return 0
 
-    take_zero = helper(low, high, num_zero, num_one, ans + "0" * num_zero)
-    take_one = helper(low, high, num_zero, num_one, ans + "1" * num_one)
+    take_zero = helper(limit, num_zero, num_one, ans + "0" * num_zero)
+    take_one = helper(limit, num_zero, num_one, ans + "1" * num_one)
     return take_zero + take_one
 
 
-print(helper(2, 3, 1, 2, ""))
+ans = 0
+low = 2
+high = 3
+zero = 1
+one = 2
+for i in range(low, high + 1):
+    ans += helper(i, zero, one, "")
+print(ans)
