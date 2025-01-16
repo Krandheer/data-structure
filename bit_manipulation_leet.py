2c351ffce509e62e42cf6d126741c57243a3b177
randheer <randheer.dakshana15@gmail.com>
2025-01-16 11:47:40 +0530

    leetcode daily challenge, medium

--- bit_manipulation_leet.py
+++ bit_manipulation_leet.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from typing import List
+
+
 def minimizeXor(num1: int, num2: int) -> int:
     set_bits = 0
     while num2:
@@ -20,3 +24,20 @@ def minimizeXor(num1: int, num2: int) -> int:
                 return x
 
     return x
+
+
+def xorAllNums(nums1: List[int], nums2: List[int]) -> int:
+    ans = 0
+    freq = defaultdict(int)
+    len1, len2 = len(nums1), len(nums2)
+    for num in nums1:
+        freq[num] = freq.get(num, 0) + len2
+
+    for num in nums2:
+        freq[num] = freq.get(num, 0) + len1
+
+    for k, v in freq.items():
+        if v & 1:
+            ans ^= k
+
+    return ans
