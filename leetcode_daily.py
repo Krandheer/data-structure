16e5f31e182d179567fd513ce7fb9a3cae128e57
randheer <randheer.dakshana15@gmail.com>
2025-01-23 13:46:47 +0530

    leetcode daily challenge, medium

--- leetcode_daily.py
+++ leetcode_daily.py
@@ -753,3 +753,21 @@ def highestPeak(isWater: List[List[int]]) -> List[List[int]]:
                 q.append((dx, dy))
 
     return heights
+
+
+def countServers(grid: List[List[int]]) -> int:
+    m, n, connected = len(grid), len(grid[0]), 0
+    rows, col = [0] * m, [0] * n
+
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1:
+                rows[i] += 1
+                col[j] += 1
+
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1 and (rows[i] > 1 or col[j] > 1):
+                connected += 1
+
+    return connected
