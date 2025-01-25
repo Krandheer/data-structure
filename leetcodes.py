9de87b3c0654d06814e65331644e659438056704
randheer <randheer.dakshana15@gmail.com>
2025-01-26 00:43:00 +0530

    montonic stack problem

--- leetcodes.py
+++ leetcodes.py
@@ -42,4 +42,51 @@ def lengthOfLongestSubstring(s: str) -> int:
     return maxi
 
 
-print(lengthOfLongestSubstring("abcabcbb"))
+# tags: leetcode: 424, quite good problem
+def characterReplacement(s: str, k: int) -> int:
+    temp = defaultdict(int)
+    l, r = 0, 0
+    ans = 0
+    maxi = 0
+    while r < len(s):
+        m = r - l + 1
+        temp[s[r]] += 1
+        maxi = max(maxi, temp[s[r]])
+        if m - maxi > k:
+            temp[s[l]] -= 1
+            l += 1
+        ans = max(ans, r - l + 1)
+        r += 1
+
+    return ans
+
+
+def checkInclusion(s1: str, s2: str) -> bool:
+    s1_map = defaultdict(int)
+    for i in s1:
+        s1_map[i] += 1
+
+    n = len(s1)
+
+    l, r = 0, n
+    while r <= len(s2):
+        temp = defaultdict(int)
+        for i in range(l, r):
+            temp[s2[i]] += 1
+        if temp == s1_map:
+            return True
+        l += 1
+        r += 1
+    return False
+
+
+def dailyTemperatures(temperatures: List[int]) -> List[int]:
+    # monotonic stack, push element while maintaining monotonicity, pop elements that voilate
+    # monotonocity
+    stack = []
+    for i in range(len(temperatures)):
+        while stack and temperatures[i] > temperatures[stack[-1]]:
+            idx = stack.pop()
+            temperatures[idx] = i - idx
+        stack.append(i)
+    return temperatures
