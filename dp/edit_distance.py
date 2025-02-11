5a474f07781fa465379f30d9b0d50283311d0f60
randheer <randheer.dakshana15@gmail.com>
2025-02-11 19:57:52 +0530

    code refactor

--- dp/edit_distance.py
+++ dp/edit_distance.py
@@ -9,14 +9,13 @@ def compare_string(index1, index2, word1, word2):
     if word1[index1] == word2[index2]:
         return compare_string(index1 - 1, index2 - 1, word1, word2)
     else:
-        return 1 + min(
-            compare_string(index1 - 1, index2 - 1, word1, word2),
-            compare_string(index1 - 1, index2, word1, word2),
-            compare_string(index1, index2 - 1, word1, word2),
-        )
+        replace = compare_string(index1 - 1, index2 - 1, word1, word2)
+        delete = compare_string(index1 - 1, index2, word1, word2)
+        insert = compare_string(index1, index2 - 1, word1, word2)
+        return 1 + min(replace, delete, insert)
 
 
-def recur(word1, word2, i, j, dp, mini):
+def recur(word1, word2, i, j, dp):
     if i < 0:
         return j + 1
     if j < 0:
@@ -26,21 +25,19 @@ def recur(word1, word2, i, j, dp, mini):
 
     # not take case
     if word1[i] == word2[j]:
-        mini = min(mini, recur(word1, word2, i - 1, j - 1, dp, mini))
+        dp[i][j] = recur(word1, word2, i - 1, j - 1, dp)
+        return dp[i][j]
     else:
-        mini = 1 + min(
-            mini,
-            recur(word1, word2, i - 1, j, dp, mini),
-            recur(word1, word2, i, j - 1, dp, mini),
-            recur(word1, word2, i - 1, j - 1, dp, mini),
-        )
-    dp[i][j] = mini
-    return dp[i][j]
+        replace = compare_string(i - 1, j - 1, word1, word2)
+        delete = compare_string(i - 1, j, word1, word2)
+        insert = compare_string(i, j - 1, word1, word2)
+        dp[i][j] = 1 + min(replace, delete, insert)
+        return dp[i][j]
 
 
 word1 = "intention"
 word2 = "execution"
 dp = [[-1] * len(word2) for _ in range(len(word1))]
-res = recur(word1, word2, len(word1) - 1, len(word2) - 1, dp, math.inf)
+res = recur(word1, word2, len(word1) - 1, len(word2) - 1, dp)
 print(res)
 print(compare_string(len(word1) - 1, len(word2) - 1, word1, word2))
