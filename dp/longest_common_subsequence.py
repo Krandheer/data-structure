0e683578c2f60d4fc4a9cddc5155d8ee5ec2c0b4
randheer <randheer.dakshana15@gmail.com>
2025-02-11 14:36:03 +0530

    optimised solution

--- dp/longest_common_subsequence.py
+++ dp/longest_common_subsequence.py
@@ -1,13 +1,3 @@
-"""
-to get the longest common string convert the solution to bottom up, and start tracking 
-from right bottom in table as that is where you get the solution and then move back to 
-left up and find if match then take otherwise move to left or up whichever is highest 
-as that is from where the current value must have come, and if match move diagonal up.
-for longest common substring start with n+1, m+1 table and fill if match with diagonal+1  
-or 0 and return max_val for table
-"""
-
-
 def lcs(index1, index2, string1, string2):
     if index1 < 0 or index2 < 0:
         return 0
@@ -43,7 +33,5 @@ def lcs_dp(index1, index2, string1, string2, dp):
         return dp[index1][index2]
 
 
-dp = [[-1 for j in range(len(s2))] for i in range(len(s1))]
+dp = [[-1] * (len(s2)) for i in range(len(s1))]
 print(lcs_dp(len(s1) - 1, len(s2) - 1, s1, s2, dp))
-for i in dp:
-    print(i)
