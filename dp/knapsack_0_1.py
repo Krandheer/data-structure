76487b58c157da944c7a8127612e11b0567e552e
randheer <randheer.dakshana15@gmail.com>
2025-02-11 12:56:11 +0530

    knapsack dp

--- dp/knapsack_0_1.py
+++ dp/knapsack_0_1.py
@@ -19,6 +19,24 @@ def knapsack(index, wt, val, bag_weight):
     return max(not_picked, picked)
 
 
+def knapsack_dp(index, wt, val, bag_weight, dp):
+    if index == 0 and bag_weight < wt[0]:
+        return 0
+    elif index == 0:
+        return val[0]
+    if dp[index][bag_weight] != -1:
+        return dp[index][bag_weight]
+    not_picked = knapsack_dp(index - 1, wt, val, bag_weight, dp)
+    picked = -math.inf
+
+    if wt[index] <= bag_weight:
+        picked = val[index] + knapsack_dp(
+            index - 1, wt, val, bag_weight - wt[index], dp
+        )
+    dp[index][bag_weight] = max(not_picked, picked)
+    return dp[index][bag_weight]
+
+
 # knapsack where picking one element many times is allowed
 def unbounded_knapsack(index, wt, val, bag_weight):
     if index == 0 and bag_weight < wt[0]:
@@ -36,9 +54,11 @@ def unbounded_knapsack(index, wt, val, bag_weight):
 wt = [3, 2, 5]
 val = [30, 40, 60]
 bag_weight = 6
+dp = [[-1] * (bag_weight + 1) for _ in range(len(wt))]
 print(knapsack(len(wt) - 1, wt, val, bag_weight))
+print(knapsack_dp(len(wt) - 1, wt, val, bag_weight, dp))
 
-wt2 = [2, 4, 6]
-val2 = [5, 11, 13]
-bag_weight2 = 10
-print(unbounded_knapsack(len(wt2) - 1, wt2, val2, bag_weight2))
+# wt2 = [2, 4, 6]
+# val2 = [5, 11, 13]
+# bag_weight2 = 10
+# print(unbounded_knapsack(len(wt2) - 1, wt2, val2, bag_weight2))
