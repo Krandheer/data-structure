0e8eb2ec329b46e94ea4b7d75f0fd64354340183
randheer <randheer.dakshana15@gmail.com>
2025-02-13 11:17:01 +0530

    decorator

--- decorator.py
+++ decorator.py
@@ -1,47 +1,4 @@
-# import itertools
-
-
-# for i in dir(itertools):
-#     if not (i.startswith("__") or i.startswith("_")):
-#         print(i)
-
-# arr = [
-#     1,
-#     2,
-#     3,
-#     4,
-# ]
-# print(arr.index(3))
-# print(arr.count(3))
-
-from collections import defaultdict
-
-# to use defaultdict behaviour need to pass the value type as
-# argument otherwise it will throw error for unknow key
-d = defaultdict(int)
-
-from itertools import product
-
-a = [1, 2, 3]
-b = [3, 4]
-
-# gives cartesian product of a and b
-prod = product(a, b)
-
-# for i in prod:
-#     print(i)
-# per = itertools.permutations(a)
-
-# while handling the json in pthon i can pass indent = 4 arguments
-# for better indentation, loads and dumps works in place, load and dupm work on file
-
-# import secrets
-
-# print(secrets.randbelow(10))
-# print(secrets.token_hex())
-
-
-## decorators are used to extend the functionality of the function on which it is used.
+# decorators are used to extend the functionality of the function on which it is used.
 
 
 def start_end_decorator(func):
@@ -59,10 +16,3 @@ def print_name():
 
 
 print_name()
-
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-import itertools
-
-# prefix sum
-print(list(itertools.accumulate(arr)))
