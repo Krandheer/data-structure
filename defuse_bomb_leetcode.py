76c2c3308175981e6c253ebc4ac4b6a43bb8b7ed
randheer <randheer.dakshana15@gmail.com>
2025-02-13 12:15:25 +0530

    decorator

--- defuse_bomb_leetcode.py
+++ defuse_bomb_leetcode.py
@@ -1,21 +1,23 @@
 from typing import List
 
+
 def process(i, j, temp, code, ans, is_negative):
     n = len(code)
-    m= 1
+    m = 1
     while m < n:
         temp -= code[i]
         if is_negative:
-            j+=1
+            j += 1
         if j >= n:
-            j = (j % n)
+            j = j % n
         temp += code[j]
         ans[m] = temp
         i += 1
         if not is_negative:
-            j+=1
+            j += 1
         m += 1
     return ans
+
 
 def decrypt(code: List[int], k: int) -> List[int]:
     n = len(code)
@@ -26,14 +28,14 @@ def decrypt(code: List[int], k: int) -> List[int]:
         i, j = 1, k + 1
         temp = sum(code[i:j])
         ans[0] = temp
-        ans = process(i,j, temp, code, ans, False)
+        ans = process(i, j, temp, code, ans, False)
     if k < 0:
         i, j = -1, k
         temp = sum(code[j:])
-        ans[0]=temp
-        ans = process(j,i, temp, code, ans, True)
+        ans[0] = temp
+        ans = process(j, i, temp, code, ans, True)
 
     return ans
 
 
-print(decrypt([2,4,9,3], -2))
+print(decrypt([2, 4, 9, 3], -2))
