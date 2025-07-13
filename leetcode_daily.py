175508b9911a9557c51e45d99deb52536983b964
randheer <randheer.dakshana15@gmail.com>
2025-07-13 13:01:55 +0530

    leetcode daily

--- leetcode_daily.py
+++ leetcode_daily.py
@@ -1736,3 +1736,20 @@ def earliestAndLatest(n: int, firstPlayer: int, secondPlayer: int) -> List[int]:
 
     count((1 << n) - 1, 0, n - 1, 1)
     return [mn, mx]  # type: ignore
+
+
+def matchPlayersAndTrainers(players: List[int], trainers: List[int]) -> int:
+    players.sort()
+    trainers.sort()
+
+    count = 0
+    i, j = 0, 0
+    while i < len(players) and j < len(trainers):
+        if players[i] <= trainers[j]:
+            count += 1
+            i += 1
+            j += 1
+        else:
+            j += 1
+
+    return count
