9a2a54df97754d6fa8fd2bfc3f56840014299f7d
randheer <randheer.dakshana15@gmail.com>
2025-07-18 23:57:13 +0530

    leetcode daily

---
+++ max_sum_subsequence.py
@@ -0,0 +1,20 @@
+import heapq
+from typing import List
+
+
+# a good problem, if you want min sum instead use max-heap
+# most subsequence problem are about dp because we can't use window or two pointer
+# but many time they could be solved using some trick like here.
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        max_heap = [(x, i) for i, x in enumerate(nums[:k])]
+        heapq.heapify(max_heap)
+        total = sum(nums[:k])
+
+        for i in range(k, len(nums)):
+            heapq.heappush(max_heap, (nums[i], i))
+            total += nums[i]
+            total -= heapq.heappop(max_heap)[0]
+
+        max_heap.sort(key=lambda x: x[1])
+        return [i for i, x in max_heap]
