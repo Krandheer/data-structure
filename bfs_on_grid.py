6436a5381b877c0451465adb4a08e9ac9c08af8b
randheer <randheer.dakshana15@gmail.com>
2025-07-20 15:45:48 +0530

    bfs on grid

--- bfs_on_grid.py
+++ bfs_on_grid.py
@@ -4,16 +4,16 @@ from collections import deque
 def is_valid(grid, visited, x, y):
     row = len(grid)
     col = len(grid[0])
-    if x < 0 or y < 0 or x >= row or y >= col or visited[x][y] == 1:
-        return False
-    return True
+    if 0 <= x < row and 0 <= y < col and not visited[x][y]:
+        return True
+    return False
 
 
 def bfs(grid, visited, node):
     queue = deque()
     queue.append(node)
     i, j = node[0], node[1]
-    visited[i][j] = 1
+    visited[i][j] = True
     while queue:
         x, y = queue.popleft()
         print(grid[x][y])
@@ -24,13 +24,7 @@ def bfs(grid, visited, node):
 
 
 grid = [[3, 1, 5], [7, 8, 2], [14, 11, 9]]
-row = len(grid)
-col = len(grid[0])
-visited = []
-for i in range(row):
-    temp = []
-    for j in range(col):
-        temp.append(0)
-    visited.append(temp)
+row, col = len(grid), len(grid[0])
+visited = [[False] * col for _ in range(row)]
 
 bfs(grid, visited, (0, 0))
