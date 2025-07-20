85b4515d7559dda0bc2e0fbc1f063fbc74d30d63
randheer <randheer.dakshana15@gmail.com>
2025-07-20 15:41:40 +0530

    leetcode daily

--- dfs_on_grid.py
+++ dfs_on_grid.py
@@ -1,21 +1,21 @@
-def is_valid(grid, visited, i, j):
+def is_valid(i, j, grid, visited):
     row, col = len(grid), len(grid[0])
-    if 0 <= i < row and 0 <= j < col and visited[i][j] == 0:
+    if 0 <= i < row and 0 <= j < col and not visited[i][j]:
         return True
     return False
 
 
 def dfs(grid, visited, start_node):
     i, j = start_node[0], start_node[1]
-    visited[i][j] = 1
+    visited[i][j] = True
     print(grid[i][j])
     for x, y in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
-        if is_valid(grid, visited, i + x, j + y):
+        if is_valid(i + x, j + y, grid, visited):
             dfs(grid, visited, (i + x, j + y))
 
 
 grid = [[3, 1, 5], [7, 8, 2], [14, 11, 9]]
-row = len(grid)
-col = len(grid[0])
-visited = [[0] * col for _ in range(row)]
+row, col = len(grid), len(grid[0])
+visited = [[False] * col for _ in range(row)]
+
 dfs(grid, visited, (0, 0))
